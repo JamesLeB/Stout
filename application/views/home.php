2b8faf3e16c69fe84944da8b2f4bd1ef7df9bde1
JamesLeB <jl149@nyu.edu>
2014-07-10 16:54:25 -0400

    git adding jquery ui to home page

--- application/views/home.php
+++ application/views/home.php
@@ -1,10 +1,24 @@
 <html>
 	<head>
 		<link href="css/style.css" rel="stylesheet" />
-		<script src='js/jquery.js'></script>
+		<script src='js/jquery-1.10.2.js'></script>
+		<script src='js/jquery-ui-1.10.4.custom.js'></script>
 		<script src='js/script.js'></script>
 	</head>
 	<body>
+		<div id='wrapper'>
+			<div id='tabs'>
+				<ul>
+					<li><a href='#tab1'>Tab1</a></li>
+					<li><a href='#tab2'>Tab2</a></li>
+					<li><a href='#tab3'>Tab3</a></li>
+				</ul>
+				<div id='tab1'>first tab</div>
+				<div id='tab2'>second tab</div>
+				<div id='tab3'>third tab</div>
+			</div>
+		</div>
+<!--
 		<div id='mainBody'>
 			<div id='debug'></div>
 			<div id='mainMenu'>
@@ -12,5 +26,6 @@
 			</div>
 			<div id='mainDisplay'></div>
 		</div>
+--!>
 	</body>
 </html>
