ba0d07a709e9d3a1115933dacb732567ddfc53f0
JamesLeB <jl149@nyu.edu>
2014-07-11 08:48:03 -0400

    adding file

---
+++ application/views/develop.php
@@ -0,0 +1,7 @@
+<div class='box' id='mainBody'>
+	<div class='box' id='debug'></div>
+	<div class='box' id='mainMenu'>
+		<button id='test'>Test</button>
+	</div>
+	<div class='box' id='mainDisplay'></div>
+</div>
