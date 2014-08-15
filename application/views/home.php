84396038fe6f57b9363eca7368c06600c7e688f5
JamesLeB <james@gowanustech.com>
2014-08-15 12:31:48 -0400

    adding title to page

--- application/views/home.php
+++ application/views/home.php
@@ -1,5 +1,6 @@
 <html>
 	<head>
+		<title>Stout</title>
 		<link href='css/ui-darkness/jquery-ui-1.10.4.custom.css' rel='stylesheet' />
 		<link href='css/style.css' rel='stylesheet' />
 		<script src='js/jquery-1.10.2.js'></script>
