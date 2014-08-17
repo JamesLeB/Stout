33997e2d1ce4095f6d495e79d96183d15410e5f3
JamesLeB <jl149@nyu.edu>
2014-08-16 20:34:28 -0400

    working on market Trade pull

--- bitphp/marketTrades.php
+++ bitphp/marketTrades.php
@@ -1,6 +1,10 @@
 <?php
-	//Echo "cross browser workaround";
+	#Echo "cross browser workaround";
 	$json = file_get_contents('https://api.mintpal.com/v1/market/trades/LTC/BTC');
-	file_put_contents('marketTrades.html',$json);
-	echo "$json";
+	$obj = json_decode($json,true);
+	$keys = array_keys($obj);
+	foreach($keys as $key){
+		echo "$key<br/>";
+	}
+	#file_put_contents('marketTrades.html',$json);
 ?>
