f2e0a53d7be43850a93a800fead7906acd8e04dd
JamesLeB <jl149@nyu.edu>
2014-08-16 23:33:37 -0400

    updating standard library

--- lib/std.php
+++ lib/std.php
@@ -1,20 +1,44 @@
 <?php
 
 	function renderTable($headings,$mdataArray){
-		echo "<table>";
-		echo "<tr class='heading'>";
+		$echo = '';
+		$echo .= "<table class='queryTable'>";
+		$echo .= "<tr class='tableHeading'>";
 		foreach($headings as $h){
-			echo "<td>$h</td>";
+			$echo .= "<td>$h</td>";
 		}
-		echo "</tr>";
+		$echo .= "<td></td>";
+		$echo .= "</tr>";
+		$count = 0;
 		foreach($mdataArray as $mdata){
-			echo "<tr class='tableData'>";
+			$count++;
+			$echo .= "<tr class='tableData'>";
 				foreach($mdata as $data){
-					echo "<td>$data</td>";
+					$echo .= "<td>$data</td>";
 				}
-				echo "</tr>";
+				#$echo .= "<td><button>00</button></td>";
+				$echo .= "</tr>";
 		}
-		echo "</table>";
+		$echo .= "</table>";
+		$echo = $count == 0 ? 'No records' : $echo;
+		return $echo;
 	}
+/*
+	function processResultSet($rs){
+		$headings = array();
+		$records = array();
+		$count = 0;
+		foreach($rs->result_array() as $row){
+			$line = array();
+			$headings = array_keys($row);
+			foreach($headings as $key){
+				$line[] = $row[$key];
+			}
+			$records[] = $line;
+			$count++;
+		}
+		return array($headings,$records,$count);
+	}
+*/
 
 ?>
