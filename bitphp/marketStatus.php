c8ca58f18ea06bd37058ee28e6bc78fe554e50d6
JamesLeB <jl149@nyu.edu>
2014-08-16 20:22:15 -0400

    setup market tab for work on market trades

--- bitphp/marketStatus.php
+++ bitphp/marketStatus.php
@@ -1,6 +1,6 @@
 <?php
-	//Echo "cross browser workaround";
+	#Echo "cross browser workaround";
 	$json = file_get_contents('https://api.mintpal.com/v1/market/stats/LTC/BTC');
-	file_put_contents('marketStatus.html',$json);
+	#file_put_contents('marketStatus.html',$json);
 	echo "$json";
 ?>
