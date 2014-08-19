06c4d4f1b4896db74ab3cf3f605682738c7a0887
JamesLeB <jl149@nyu.edu>
2014-08-18 21:59:57 -0400

    rework how market data gets pulled

--- bitphp/marketTrades.php
+++ bitphp/marketTrades.php
@@ -1,10 +1,39 @@
 <?php
-	#Echo "cross browser workaround";
+	#file_put_contents('marketTrades.html',$json);
+	$echo = array();
+	$echo[] = 'Outlander';
+
+	$aa = array('one','two');
+	$bb = array('three','four');
+
+	#$jamie = renderTable($aa,$bb);
+	include("../lib/std.php");
+	$jamie = testing();
+	echo $jamie;
+
+/*
 	$json = file_get_contents('https://api.mintpal.com/v1/market/trades/LTC/BTC');
 	$obj = json_decode($json,true);
-	$keys = array_keys($obj);
+
+	$count = $obj['count'];
+	$echo[] = "Count $count";
+
+	$trades = $obj['trades'];
+
+	$echo[] = 'Trade Keys:';
+	$keys = array_keys($trades[0]);
 	foreach($keys as $key){
-		echo "$key<br/>";
+		$echo[] = "$key";
 	}
-	#file_put_contents('marketTrades.html',$json);
+
+	foreach($trades as $trade){
+		$echo[] = "$trade";
+	}
+*/
+
+	$msg = '';
+	foreach($echo as $e){
+		$msg .= "$e<br/>";
+	}
+	echo $msg;
 ?>
