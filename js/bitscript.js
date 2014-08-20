76647c6d1533323bf7876acc4faa243ac3098814
JamesLeB <james@gowanustech.com>
2014-08-20 10:27:39 -0400

    adding sample code

--- js/bitscript.js
+++ js/bitscript.js
@@ -132,6 +132,20 @@ function renderTable(headings,data){
 	r += "</tr></table>";
 	return r;
 }
+/*
+	$('#').click(function(){
+		$('#').css('background','yellow');
+		var target = 'index.php?/controller/function';
+		var request=$.post(target,'',function(data){
+			$('#').css('background','lightgreen');
+			$('#').html(data);
+			//var debug = request.getResponseHeader('debug');
+			//$('#debug').html(debug);
+			//var kara = request.getResponseHeader('kara');
+			//if( kara == 1 ){goKara();}else{}
+		});
+	});
+*/
 
 $(document).ready(function(){
 	// SETUP PAGE
