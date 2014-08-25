41c112d83c302a0742352137228eceb9f3babad2
JamesLeB <james@gowanustech.com>
2014-08-25 16:15:42 -0400

    building basic trader

--- application/views/test.php
+++ application/views/test.php
@@ -1,1 +1,8 @@
-test
+<div>
+	<button id='getMarketTrades'>getMarketTrades</button>
+	<button id='getMarketSells'>getMarketSells</button>
+	<button id='getMarketBuys'>getMarketBuys</button>
+	<div>
+		market trade data
+	</div>
+</div>
