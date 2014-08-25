05f73483b0b811416a7eb514928c428fbfb0229b
JamesLeB <james@gowanustech.com>
2014-08-25 16:29:13 -0400

    add graphing button

--- application/views/test.php
+++ application/views/test.php
@@ -2,6 +2,7 @@
 	<button id='getMarketTrades'>getMarketTrades</button>
 	<button id='getMarketSells'>getMarketSells</button>
 	<button id='getMarketBuys'>getMarketBuys</button>
+	<button id='Graph'>Graph</button>
 	<div>
 		market trade data
 	</div>
