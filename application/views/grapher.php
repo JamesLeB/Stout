c0f69cf0fe15600fd5a17924f65bbccd408a5019
JamesLeB <james@gowanustech.com>
2014-08-27 15:39:12 -0400

    working on grapher

--- application/views/grapher.php
+++ application/views/grapher.php
@@ -1,1 +1,26 @@
-this is the grapher
+<!-- Inside div #grapher --!>
+<div>
+	<button>A</button>
+	<button>B</button>
+	<button>C</button>
+</div>
+<div>
+	graph will do here
+</div>
+<script>
+</script>
+<style>
+	#grapher div {
+		margin : 5px;
+		padding : 5px;
+		border-style : solid;
+		border-width : 2px;
+	}
+	#grapher > :nth-child(1) {
+		border-color : yellow;
+	}
+	#grapher > :nth-child(2) {
+		border-color : green;
+		height : 300px;
+	}
+</style>
