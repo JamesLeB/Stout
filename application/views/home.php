e0f44bb64f361268cb277e4c69d9c60bf197213b
JamesLeB <jl149@nyu.edu>
2014-08-28 17:16:16 -0400

    addding space to home view

--- application/views/home.php
+++ application/views/home.php
@@ -1,13 +1,17 @@
 <html>
 	<head>
 		<title>Stout</title>
+
 		<link href='css/ui-darkness/jquery-ui-1.10.4.custom.css' rel='stylesheet' />
 		<link href='css/style.css' rel='stylesheet' />
+
 		<script src='js/jquery-1.10.2.js'></script>
 		<script src='js/jquery-ui-1.10.4.custom.js'></script>
 		<script src='js/script.js'></script>
 		<script src='js/bitglobal.js'></script>
+
 	</head>
+
 	<body>
 		<div id='wrapper'>
 			<div id='stoutHeading'>
@@ -17,4 +21,5 @@
 			<?php echo $slideTray ?>
 		</div>
 	</body>
+
 </html>
