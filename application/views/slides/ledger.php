4f25fd8e1e3b1b12f8cf50ac5b22c5acb0fc331f
JamesLeB <jl149@nyu.edu>
2014-08-28 17:54:08 -0400

    working on ledger view

--- application/views/slides/ledger.php
+++ application/views/slides/ledger.php
@@ -1,39 +1,52 @@
 <div id='ledger'>
 	<div>
-		<button>A</button>
-		<button>B</button>
+		<button>Setup</button>
+		<button>Form</button>
+		<button>Load</button>
+	</div>
+	<div>
+		form goes here
 	</div>
 	<div>
 		ledger goes here
 	</div>
 </div>
 <script>
-	// A button
+	// Setup button
 	$('#ledger > :nth-child(1) > :nth-child(1)').click(function(){
-		$('#ledger > :nth-child(2)').html('hello');
-	});
-	// B button
-	$('#ledger > :nth-child(1) > :nth-child(2)').click(function(){
-		$('#ledger > :nth-child(2)').html('world');
-/*
-		var target = 'index.php?/grapher/config';
+		var target = 'index.php?/slides/ledger/setup';
 		var request = $.post(target,'',function(data){
-			$('#grapher > :nth-child(2)').html(data);
+			$('#ledger > :nth-child(3)').html(data);
 		});
-*/
+	});
+	// Hide Form button
+	$('#ledger > :nth-child(2)').hide();
+	$('#ledger > :nth-child(1) > :nth-child(2)').click(function(){
+		$('#ledger > :nth-child(2)').toggle();
+	});
+	// LoadLedger button
+	$('#ledger > :nth-child(1) > :nth-child(3)').click(function(){
+		var target = 'index.php?/slides/ledger/loadLedger';
+		var request = $.post(target,'',function(data){
+			$('#ledger > :nth-child(3)').html(data);
+		});
 	});
 </script>
 <style>
 	#ledger div {
 		margin : 5px;
 		padding : 5px;
-		border-style : solid;
+		border-style : ridge;
 		border-width : 2px;
 	}
 	#ledger > :nth-child(1) {
 		border-color : yellow;
 	}
 	#ledger > :nth-child(2) {
+		border-color : red;
+		height : 100px;
+	}
+	#ledger > :nth-child(3) {
 		border-color : green;
 		height : 300px;
 	}
