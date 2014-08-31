38ec9f0a23635f87a3c8a2c3cc362f8b5b900a28
JamesLeB <jl149@nyu.edu>
2014-08-30 23:09:18 -0400

    removing weeds

--- application/views/slideTray.php
+++ application/views/slideTray.php
@@ -17,21 +17,4 @@
 	}
 	$html .= '</div>';
 	echo $html;
-/*
-<div id='tabs'>
-	<ul>
-
-		<li><a href='#grapher'>Grapher</a></li>
-		<li><a href='#trader'>Trader</a></li>
-		<li><a href='#exchange'>Exchange</a></li>
-		<li><a href='#development'>Development</a></li>
-		<li><a href='#production'>Production</a></li>
-	</ul>
-	<div id='grapher'><?php echo $grapher ?></div>
-	<div id='exchange'><?php echo $exchange ?></div>
-	<div id='trader'><?php echo $trader ?></div>
-	<div id='development'><?php echo $development ?></div>
-	<div id='production'><?php echo $production ?></div>
-</div>
-*/
 ?>
