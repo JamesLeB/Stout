18891db2d3d4d51349969458b6525fd45bd047cb
JamesLeB <jl149@nyu.edu>
2014-09-07 17:11:12 -0400

    seperating trader code from general code

--- js/script.js
+++ js/script.js
@@ -148,26 +148,8 @@ function getMarketStatus(){
 }
 
 $(document).ready(function(){
-	/*
-	*	Stout Heading code
-	*/
-		setInterval(function(){
-			var t = new Date();
-			var M = t.getMonth()+1;
-			var d = t.getDate();
-			var y = t.getFullYear();
-			var h = t.getHours();
-			var m = t.getMinutes();
-			var s = t.getSeconds();
-			$('clock').html(y+'-'+M+'-'+d+'<br/>'+h+':'+m+':'+s); 
-		},1000);
-	/*
-	*	END Stout Heading code
-	*/
 
 	// SETUP PAGE
-	$("#tabs").tabs();
-	$("#accordion").accordion();
 	loadSales();
 	loadInventory();
 	loadOpenBids();
