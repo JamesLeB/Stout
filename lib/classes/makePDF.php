10b51651fb8ecbd393254e377662e7b2df0a5ed3
JamesLeB <james@gowanustech.com>
2014-09-11 10:57:40 -0400

    add ability to create pdf file

--- lib/classes/makePDF.php
+++ lib/classes/makePDF.php
@@ -1,6 +1,6 @@
 <?php
 class makePDF {
-	private $sceneCount = 4;
+	private $sceneCount = 1;
 	function getSceneCount(){ return $this->sceneCount; }
 	function stage(){
 		$ms = '';
@@ -11,9 +11,26 @@ class makePDF {
 		else{ $ms .= 'ERROR - invalid scene number!!'; }
 		return $ms;
 	}
-	function stage1(){ return 'stage1'; }
+	function stage1(){
+		$ms = '';
+		$ms .= 'start<br/>';
+		$this->createPDF();
+		$ms .= 'end<br/>';
+		return $ms;
+	}
 	function stage2(){ return 'stage2'; }
 	function stage3(){ return 'stage3'; }
 	function stage4(){ return 'stage4'; }
+	function createPDF(){
+		require('lib/pdf/fpdf.php');
+		$pdf = new FPDF();
+		$pdf->AddPage();
+		$pdf->SetFont('Arial','B',12);
+		$pdf->SetTextColor(128);
+		$pdf->Cell(40,10,'MEETING TIME','B');
+		$pdf->ln();
+		$pdf->Cell(40,10,'JR IS BACK');
+		$pdf->Output('files/test1.pdf','F');
+	}
 }
 ?>
