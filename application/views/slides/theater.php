a60b1fd5bdce979f93f0b6b844beb9acafaf0e93
JamesLeB <james@gowanustech.com>
2014-09-11 11:51:39 -0400

    formatting theater view

--- application/views/slides/theater.php
+++ application/views/slides/theater.php
@@ -3,14 +3,25 @@
 		<button>GO</button>
 	</div>
 	<div>
+		Status
+	</div>
+	<div>
 		Return
 	</div>
 </div>
 <style>
+	#theater div{
+		border-style : solid;
+		border-color : gray;
+		border-width ; 1px;
+		padding : 10px;
+		margin : 10px;
+	}
 </style>
 <script>
 	// GO button
-	var theaterReturn = '#theater div:nth-child(2)';
+	var theaterStatus = '#theater div:nth-child(2)';
+	var theaterReturn = '#theater div:nth-child(3)';
 	$('#theater div button').click(function(){
 		var target = 'index.php?/stage/setup';
 		var request = $.post(target,'',function(data){
@@ -22,9 +33,14 @@
 	function stage(){
 		var target = 'index.php?/stage/running';
 		var request = $.post(target,'',function(data){
-			$(theaterReturn).append(data);
 			var scene = request.getResponseHeader('scene');
 			if( scene == 1 ){stage();}
+			var status = request.getResponseHeader('status');
+			if( status == 1 ){
+				$(theaterStatus).html(data);
+			}else{
+				$(theaterReturn).append(data);
+			}
 		});
 	}
 </script>
