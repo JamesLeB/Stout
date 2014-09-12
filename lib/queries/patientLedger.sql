6ce9ec21b1d4cfd0d19999cd90a342829ca5dc85
JamesLeB <james@gowanustech.com>
2014-09-12 19:55:35 -0400

    adding new patientLedger query

--- lib/queries/patientLedger.sql
+++ lib/queries/patientLedger.sql
@@ -2,7 +2,12 @@
 	This query will pull all ledger entires
 	parms   startdate  enddate  patient
 */
+
+declare @chart as varchar(32)
+set @chart = '072569'
+	
 IF OBJECT_ID('tempdb..#DATA') IS NOT NULL DROP TABLE #DATA
+IF OBJECT_ID('tempdb..#CLAIMS') IS NOT NULL DROP TABLE #CLAIMS
 
 SELECT
 	pl.PROC_LOGID,
@@ -20,9 +25,20 @@ SELECT
 	pl.AMOUNT * .01 AS amount,
 	CASE
 		WHEN pl.CHART_STATUS = 102 THEN pc.ADACODE
+		WHEN pl.CHART_STATUS = 90 AND pl.CLASS = 1 THEN 'Payment'
+		WHEN pl.CHART_STATUS = 90 AND pl.CLASS = 2 THEN 'Adjustment'
+		WHEN pl.CHART_STATUS = 90 AND pl.CLASS = 3 THEN 'Dent Ins.'
 		ELSE '--'
 	END AS ADACODE,
-	pc.DESCRIPTION,
+	CASE
+		WHEN pl.CHART_STATUS = 102 THEN pc.DESCRIPTION
+		WHEN pl.CHART_STATUS = 90 AND pl.CLASS = 1 THEN
+			(SELECT d.Description FROM DDB_Def_RO AS d WHERE pl.ORD = d.DefID AND d.Type = 6)
+		WHEN pl.CHART_STATUS = 90 AND pl.CLASS = 2 THEN
+			(SELECT d.Description FROM DDB_Def_RO AS d WHERE pl.ORD = d.DefID AND d.Type = 9)
+		WHEN pl.CHART_STATUS = 90 AND pl.CLASS = 3 THEN 'Prim Dent Ins. Payment'
+		ELSE '--'
+	END AS description,
 	clinic.RSCID AS clinic,
 	clinic.PRACTITLE,
 	pat.CHART,
@@ -32,7 +48,17 @@ SELECT
 	CASE
 		WHEN pat.GENDER = 1 THEN 'M'
 		ELSE 'F'
-	END AS gender
+	END AS gender,
+	CASE
+		WHEN pl.TOOTH_RANGE_START = 0 AND pl.SURF_STRING <> '' THEN pl.SURF_STRING
+		WHEN pl.TOOTH_RANGE_START = 0 AND pl.SURF_STRING = '' THEN ''
+		ELSE CAST(pl.TOOTH_RANGE_START AS VARCHAR(2))
+	END as tooth,
+	CASE
+		WHEN pl.CHART_STATUS = 102 THEN (SELECT RSCID FROM DDB_RSC AS r WHERE pl.PROVID = r.URSCID)
+		ELSE ''
+	END as provider,
+	pl.SURF_STRING as test
 INTO
 	#DATA
 FROM
@@ -42,7 +68,7 @@ FROM
 	JOIN DDB_PAT AS pat ON pl.PATID = pat.PATID
 WHERE
 	pl.CREATEDATE >= '2009-09-01'
-	AND pat.CHART = ?
+	AND pat.CHART = @chart
 	AND (
 		(pl.CHART_STATUS = 102) OR
 		(pl.CHART_STATUS = 90 AND pl.CLASS = 1) OR
@@ -50,4 +76,44 @@ WHERE
 		(pl.CHART_STATUS = 90 AND pl.CLASS = 3)
 	)
 
-SELECT * FROM #DATA
+SELECT
+	CASE
+		WHEN cl.STATUS = 2 THEN CAST(cl.DATERECEIVED AS DATE)
+		ELSE CAST(cl.DATEOFCLAIM AS DATE)
+	END AS createDate,
+	'                  ' AS tooth,
+	'Dent Ins.' AS ADACODE,
+	CASE
+		WHEN cl.STATUS = 2 THEN 'Prim Clai' + '-Received ' + CAST(cl.TOTALBILLED*.01 AS VARCHAR(16)) 
+		ELSE 'Prim Clai' + '-Received ' + CAST(cl.TOTALBILLED*.01 AS VARCHAR(16))
+	END AS DESCRIPTION,
+	cl.TOTALBILLED*.01 AS amount,
+	'             ' AS provider,
+	clinic.RSCID AS clinic,
+	'Claim              ' AS lineType
+INTO #CLAIMS
+FROM
+	DDB_CLAIM AS cl
+	JOIN DDB_PAT AS pat ON cl.PATID = pat.PATID
+	JOIN DDB_RSC AS clinic on cl.ClinicAppliedTo = clinic.URSCID
+WHERE
+	pat.CHART = @chart
+
+INSERT INTO #CLAIMS
+SELECT
+	createDate,
+	tooth,
+	ADACODE,
+	DESCRIPTION,
+	amount,
+	provider,
+	clinic,
+	lineType
+FROM #DATA
+
+SELECT
+	*
+FROM
+	#CLAIMS
+ORDER BY
+	createDate
