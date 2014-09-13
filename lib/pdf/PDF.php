f42408af53755d704a2358e10a120d3234200060
JamesLeB <james@gowanustech.com>
2014-09-12 23:37:09 -0400

    use align array to set alignment in heading

--- lib/pdf/PDF.php
+++ lib/pdf/PDF.php
@@ -9,6 +9,7 @@ class PDF extends FPDF{
 		$headings = $obj[0];
 		$patient  = $obj[1];
 		$colwidth = $obj[2];
+		$align    = $obj[3];
 		$chart = $patient['chart'];
 
 		$this->SetTextColor(0);
@@ -23,14 +24,14 @@ class PDF extends FPDF{
 
 		$this->SetTextColor(0);
 		$this->SetFont('Arial','B',8);
-		$this->Cell($colwidth[0],10,$headings[0]);
-		$this->Cell($colwidth[1],10,$headings[1]);
-		$this->Cell($colwidth[2],10,$headings[2],0,0,'R');
-		$this->Cell($colwidth[3],10,'');
-		$this->Cell($colwidth[4],10,$headings[3]);
-		$this->Cell($colwidth[5],10,$headings[4]);
-		$this->Cell($colwidth[6],10,$headings[5]);
-		$this->Cell($colwidth[7],10,$headings[6]);
+		$this->Cell($colwidth[0],10,$headings[0],0,0,$align[0]);
+		$this->Cell($colwidth[1],10,$headings[1],0,0,$align[1]);
+		$this->Cell($colwidth[2],10,$headings[2],0,0,$align[2]);
+		$this->Cell($colwidth[3],10,$headings[3],0,0,$align[3]);
+		$this->Cell($colwidth[4],10,$headings[4],0,0,$align[4]);
+		$this->Cell($colwidth[5],10,'');
+		$this->Cell($colwidth[6],10,$headings[5],0,0,$align[5]);
+		$this->Cell($colwidth[7],10,$headings[6],0,0,$align[6]);
 		$this->ln();
 	}
 }
