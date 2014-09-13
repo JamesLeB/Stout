3d4be6c54e63630b0a630f7df2ee27c3d937e71d
JamesLeB <jl149@nyu.edu>
2014-09-13 13:30:16 -0400

    cleaing up file

--- application/views/home.php
+++ application/views/home.php
@@ -1,21 +1,16 @@
 <html>
 	<head>
 		<title>Stout</title>
-
 		<link href='css/ui-darkness/jquery-ui-1.10.4.custom.css' rel='stylesheet' />
 		<link href='css/style.css' rel='stylesheet' />
-
 		<script src='js/jquery-1.10.2.js'></script>
 		<script src='js/jquery-ui-1.10.4.custom.js'></script>
-
 		<script src='js/docReady.js'></script>
 <!--
 		<script src='js/script.js'></script>
 		<script src='js/bitglobal.js'></script>
 --!>
-
 	</head>
-
 	<body>
 		<div id='wrapper'>
 			<div id='stoutHeading'>
@@ -28,5 +23,4 @@
 		<style>
 		</style>
 	</body>
-
 </html>
