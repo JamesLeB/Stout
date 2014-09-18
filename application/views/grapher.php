c35b47557758c112164e3ce50ca0b8e2d4d93dcf
JamesLeB <jl149@nyu.edu>
2014-09-17 21:16:08 -0400

    switching script and style code

--- application/views/grapher.php
+++ application/views/grapher.php
@@ -9,6 +9,22 @@
 		Main return area
 	</div>
 </div>
+<style>
+	#grapher div {
+		margin : 5px;
+		padding : 5px;
+		border-style : solid;
+		border-width : 2px;
+	}
+	#grapher > :nth-child(1) {
+		border-color : yellow;
+	}
+	#grapher > :nth-child(2) {
+		border-color : green;
+		height : 400px;
+		padding : 20px;
+	}
+</style>
 <script>
 	// SETTINGS
 	//var defaultGraph = '<img src="lib/graphs/defaultGraph.php" />';
@@ -32,19 +48,3 @@
 		});
 	});
 </script>
-<style>
-	#grapher div {
-		margin : 5px;
-		padding : 5px;
-		border-style : solid;
-		border-width : 2px;
-	}
-	#grapher > :nth-child(1) {
-		border-color : yellow;
-	}
-	#grapher > :nth-child(2) {
-		border-color : green;
-		height : 400px;
-		padding : 20px;
-	}
-</style>
