508ceaab132715fb5a9cdc3c1fd9ea2189878546
JamesLeB <jl149@nyu.edu>
2014-09-24 18:07:35 -0400

    added comments

--- lib/std.php
+++ lib/std.php
@@ -1,12 +1,15 @@
 <?php
+	# These are utility functions that stout uses
 
 	function custSort($a,$b){
+		# sorts the price time series from mintpal
 		if($a['time'] == $b['time']){
 			return 0;
 		}
 		return ($a['time'] < $b['time']) ? -1 : 1;
 	}
 	function renderTable($headings,$mdataArray){
+		# this gets used a lot, a quick little way to get sql data to html
 		$echo = '';
 		$echo .= "<table class='queryTable'>";
 		$echo .= "<tr class='tableHeading'>";
@@ -28,8 +31,12 @@
 		$echo .= "</table>";
 		#$echo = $count == 0 ? 'No records' : $echo;
 		return $echo;
-	}
+	} # END of renderTable function
 	function processResultSet($rs){
+		# another utility that get used offten
+		# as part of a quick way to get sql data to the screen
+		# takes the result set returned from the query and
+		# turns it into something the rendertable fuuntion can use
 		$headings = array();
 		$records = array();
 		$count = 0;
@@ -44,5 +51,4 @@
 		}
 		return array($headings,$records,$count);
 	}
-
 ?>
