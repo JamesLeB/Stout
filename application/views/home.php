c0ffa67bca766859ed934d3b14142ef5cae93d49
JamesLeB <jl149@nyu.edu>
2014-09-24 18:17:54 -0400

    remove script tags from home page

--- application/views/home.php
+++ application/views/home.php
@@ -1,16 +1,21 @@
 <html>
 	<head>
+
 		<title>Stout</title>
+
 		<link href='css/ui-darkness/jquery-ui-1.10.4.custom.css' rel='stylesheet' />
 		<link href='css/style.css' rel='stylesheet' />
+
 		<script src='js/jquery-1.10.2.js'></script>
 		<script src='js/jquery-ui-1.10.4.custom.js'></script>
+
 		<script src='js/docReady.js'></script>
 <!--
 		<script src='js/script.js'></script>
 		<script src='js/bitglobal.js'></script>
 --!>
 	</head>
+
 	<body>
 		<div id='wrapper'>
 			<div id='stoutHeading'>
@@ -21,7 +26,6 @@
 			</div>
 			<?php echo $slideTray ?>
 		</div>
-		<style>
-		</style>
 	</body>
+
 </html>
