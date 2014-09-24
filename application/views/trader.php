59ed60b3c06847aa8fba7f434d3538b33e55113d
JamesLeB <jl149@nyu.edu>
2014-09-24 19:01:05 -0400

    rebuild trader view

--- application/views/trader.php
+++ application/views/trader.php
@@ -1,19 +1,63 @@
-<!-- IN DIV #trader --!>
 <div id='trader'>
-	<button id='getMarketTrades'>getMarketTrades</button>
-	<button id='getMarketSells'>getMarketSells</button>
-	<button id='getMarketBuys'>getMarketBuys</button>
-	<button id='Graph'>Graph</button>
-	<div>
-		market trade data
-	</div>
+	<table>
+		<tr>
+			<td id='trader_Icon'>
+				<img src='lib/images/cricket.png' />
+			</td>
+			<td id='trader_Status'>Status</td>
+		</tr>
+		<tr>
+			<td id='trader_Controls'>
+				<button>TEST</button>
+			</td>
+			<td id='trader_View'>View</td>
+		</tr>
+	</table>
 </div>
 <style>
-	#trader img{
-		margin : 10px;
-		padding : 10px;
-		border-color : gray;
-		border-style : dotted;
-		border-width : 1px;
+	#trader td{
+		border-color : #007A00;
+		border-style : dashed;
+		border-width: 1px;
+		padding : 5px;
+	}
+	#trader_Icon{
+		background : white;
+		text-align : center;
+	}
+	#trader_Icon img{
+		height : 50px;
+	}
+	#trader_Controls{
+		vertical-align : top;
+		width : 120px;
+		text-align : center;
+	}
+	#trader_Controls button{
+		width : 90px;
+	}
+	#trader_View{
+		vertical-align : top;
+		height : 400px;
+		width : 800px;
+		text-align : left;
+		font-size : 1.5em;
+		color : #66AF66;
 	}
 </style>
+<script>
+	var trader_Target = 'index.php?/slides/trader/';
+	// Test button
+	$('#trader_Controls button:nth-child(1)').click(function(){
+		/*
+		var func = 'test';
+		var request = $.post(workerTarget+func,'',function(data){
+			$('#workerView').html(data);
+			//$('#workerView').html('TEST Button disabled');
+			//var scene = request.getResponseHeader('scene');
+			//if( scene == 1 ){stage();}
+		});
+		*/
+			$('#trader_View').html('TEST Button disabled');
+	});
+</script>
