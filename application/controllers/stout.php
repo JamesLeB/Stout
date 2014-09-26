4a80140c680cbe696569d62065d37e56bd6a6b4c
JamesLeB <jl149@nyu.edu>
2014-09-25 21:32:30 -0400

    reorder slides

--- application/controllers/stout.php
+++ application/controllers/stout.php
@@ -17,8 +17,8 @@ class Stout extends CI_Controller {
 		$form['block2'] = $this->load->view('dnd/newCharFormBlock2','',true);
 */
 		$slide = array();
-		$slide[] = array('Worker',     $this->load->view('slides/worker','',true));
 		$slide[] = array('Trader',     $this->load->view('slides/trader','',true));
+		$slide[] = array('Worker',     $this->load->view('slides/worker','',true));
 		$slide[] = array('Grapher',    $this->load->view('grapher','',true));
 /*
 		$slide[] = array('Stage',      $this->load->view('slides/stage','',true));
