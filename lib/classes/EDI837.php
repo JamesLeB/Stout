5cfd31b8de8b81ba54c5248548df9ace95a5f661
JamesLeB <james@gowanustech.com>
2014-10-16 16:22:20 -0400

    adjusting steps to complete edi conversion

--- lib/classes/EDI837.php
+++ lib/classes/EDI837.php
@@ -6,9 +6,10 @@ class EDI837{
 	public function loadEDI837D(){
 		$m = array();
 		$m[] = "Step 1..Get x12 file from Axium";
-		$m[] = "Step 2..Read file and load into object";
-		$m[] = "Step 3..Create 837I";
-		$m[] = "Step 4..Write 837I to disk";
+		$m[] = "Step 2..Read file and load into 837D object";
+		$m[] = "Step 3..Convert 837D to 837I";
+		$m[] = "Step 3..Create x12 file from 837I object";
+		$m[] = "Step 4..Write x12 to disk";
 		$e = ''; foreach($m as $mm){$e .= "$mm<br/>";}
 		return "$e";
 	}
