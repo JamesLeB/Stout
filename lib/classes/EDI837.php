a5cfa9463c38d7ffebe83a6644675b9092d719ae
JamesLeB <james@gowanustech.com>
2014-10-17 10:15:29 -0400

    start work on script to open read x12 file

--- lib/classes/EDI837.php
+++ lib/classes/EDI837.php
@@ -1,15 +1,48 @@
 <?php
 class EDI837{
+
+	private $filepath;
+
+	function __construct(){
+		$this->filepath = 'files/EDIbatches/';
+	}
+
 	public function test(){
-		return "hello from EDI837 class";
+		return "Testing EDI837";
 	}
 	public function loadEDI837D(){
 		$m = array();
 		$m[] = "Step 1..Get x12 file from Axium";
+$file = 'a.x12';
+$x12 = file_get_contents($this->filepath.$file);
+$segments = preg_split('/~/',$x12);
+#foreach($segments as $seg){ $m[] = $seg; }
+try{
+	$seg = array_shift($segments);
+	if(preg_match('/^ISA\*/',$seg)){
+		$temp = preg_split('/\*/',$seg);
+		$etin =  $temp[6];
+		$prov =  $temp[8];
+		$date =  $temp[9];
+		$time =  $temp[10];
+		$batch = $temp[13];
+		$m[] = $seg;
+		$m[] = "Etin:  $etin";
+		$m[] = "Prov:  $prov";
+		$m[] = "Date:  $date";
+		$m[] = "Time:  $time";
+		$m[] = "Batch: $batch";
+	}else{throw new exception('error loading ISA');}
+}catch(exception $e){
+	$error = $e->getMessage();
+	$m[] = "Error: $error";
+}
+/*
 		$m[] = "Step 2..Read file and load into 837D object";
 		$m[] = "Step 3..Convert 837D to 837I";
 		$m[] = "Step 3..Create x12 file from 837I object";
 		$m[] = "Step 4..Write x12 to disk";
+*/
 		$e = ''; foreach($m as $mm){$e .= "$mm<br/>";}
 		return "$e";
 	}
