58af0be3c95477e9c4a39d8e98296cddd1a8890f
JamesLeB <james@gowanustech.com>
2014-10-21 11:22:07 -0400

    continued work on the edi conversion script

--- lib/classes/EDI837.php
+++ lib/classes/EDI837.php
@@ -192,16 +192,130 @@ class EDI837{
 */
 		$e = ''; foreach($m as $mm){$e .= "$mm<br/>";}
 		return "$e";
-	}
+	} # END function loadEDI837D
 	private function loadDentalClaim(&$segments){
+
 		$claim = new dentalClaim();
-		$claim->setClaimIndex(76);
+
 		#LOAD HL
 		$seg = array_shift($segments);
 		if(preg_match('/^HL\*/',$seg)){
+			$claim->setClaimIndex($seg);
 		}else{throw new exception("error loading HL<br/>---<br/>$seg<br/>---");}
 
+		#LOAD SBR
+		$seg = array_shift($segments);
+		if(preg_match('/^SBR\*/',$seg)){
+		}else{throw new exception("error loading SBR<br/>---<br/>$seg<br/>---");}
+
+		#LOAD NM1
+		$seg = array_shift($segments);
+		if(preg_match('/^NM1\*IL\*1\*/',$seg)){
+			$temp = preg_split('/\*/',$seg);
+			$claim->setPatientFirst($temp[3]);
+			$claim->setPatientLast($temp[4]);
+			$claim->setPatientMiddle($temp[5]);
+			$claim->setPatientId($temp[9]);
+		}else{throw new exception("error loading SBR<br/>---<br/>$seg<br/>---");}
+
+		#LOAD N3
+		$seg = array_shift($segments);
+		if(preg_match('/^N3\*/',$seg)){
+			$temp = preg_split('/\*/',$seg);
+			$claim->setPatientStreet($temp[1]);
+		}else{throw new exception("error loading N3<br/>---<br/>$seg<br/>---");}
+
+		#LOAD N4
+		$seg = array_shift($segments);
+		if(preg_match('/^N4\*/',$seg)){
+			$temp = preg_split('/\*/',$seg);
+			$claim->setPatientCity($temp[1]);
+			$claim->setPatientState($temp[2]);
+			$claim->setPatientZip($temp[3]);
+		}else{throw new exception("error loading N4<br/>---<br/>$seg<br/>---");}
+
+		#LOAD DMG
+		$seg = array_shift($segments);
+		if(preg_match('/^DMG\*D8\*/',$seg)){
+			$temp = preg_split('/\*/',$seg);
+			$claim->setPatientBirth($temp[2]);
+			$claim->setPatientSex($temp[3]);
+		}else{throw new exception("error loading N4<br/>---<br/>$seg<br/>---");}
+
+		#LOAD NM1
+		$seg = array_shift($segments);
+		if(preg_match('/^NM1\*PR\*2\*/',$seg)){
+			$temp = preg_split('/\*/',$seg);
+			$claim->setPayerName($temp[3]);
+			$claim->setPayerId($temp[9]);
+		}else{throw new exception("error loading NM1<br/>---<br/>$seg<br/>---");}
+
+		#LOAD N3
+		$seg = array_shift($segments);
+		if(preg_match('/^N3\*/',$seg)){
+			$temp = preg_split('/\*/',$seg);
+			$claim->setPayerStreet1($temp[1]);
+			$claim->setPayerStreet2($temp[2]);
+		}else{throw new exception("error loading N3<br/>---<br/>$seg<br/>---");}
+
+		#LOAD N4
+		$seg = array_shift($segments);
+		if(preg_match('/^N4\*/',$seg)){
+			$temp = preg_split('/\*/',$seg);
+			$claim->setPayerCity($temp[1]);
+			$claim->setPayerState($temp[2]);
+			$claim->setPayerZip($temp[3]);
+		}else{throw new exception("error loading N4<br/>---<br/>$seg<br/>---");}
+
+		#LOAD REF
+		$seg = array_shift($segments);
+		if(preg_match('/^REF\*FY\*/',$seg)){
+			$temp = preg_split('/\*/',$seg);
+			$claim->setPayer2ndId($temp[2]);
+		}else{throw new exception("error loading REF<br/>---<br/>$seg<br/>---");}
+
+		#LOAD REF
+		$seg = array_shift($segments);
+		if(preg_match('/^REF\*G2\*/',$seg)){
+			$temp = preg_split('/\*/',$seg);
+			$claim->setBillingProvider2ndId($temp[2]);
+		}else{throw new exception("error loading REF<br/>---<br/>$seg<br/>---");}
+
+		#LOAD CLM
+		$seg = array_shift($segments);
+		if(preg_match('/^CLM\*/',$seg)){
+			$temp = preg_split('/\*/',$seg);
+			$claim->setClaimId($temp[1]);
+			$claim->setClaimAmount($temp[2]);
+			$claim->setFacilityCode($temp[5]);
+			$claim->setSignature($temp[6]);
+			$claim->setAssignmentCode($temp[7]);
+			$claim->setAssignmentStatus($temp[8]);
+			$claim->setRelease($temp[9]);
+		}else{throw new exception("error loading CLM<br/>---<br/>$seg<br/>---");}
+
+		#LOAD DTP
+		$seg = array_shift($segments);
+		if(preg_match('/^DTP\*472\*D8\*/',$seg)){
+			$temp = preg_split('/\*/',$seg);
+			$claim->setServiceDate($temp[3]);
+		}else{throw new exception("error loading DTP<br/>---<br/>$seg<br/>---");}
+
+		#LOAD REF
+		$seg = array_shift($segments);
+		if(preg_match('/^REF\*D9\*/',$seg)){
+			$temp = preg_split('/\*/',$seg);
+			$claim->setClaimId2($temp[2]);
+		}else{throw new exception("error loading REF<br/>---<br/>$seg<br/>---");}
+
+		#LOAD NTE
+		$seg = array_shift($segments);
+		if(preg_match('/^NTE\*ADD\*/',$seg)){
+			$temp = preg_split('/\*/',$seg);
+			$claim->setClaimNote($temp[2]);
+		}else{throw new exception("error loading NTE<br/>---<br/>$seg<br/>---");}
+
 		return $claim;
-	}
+	}# END function loadDentalClaim
 }
 ?>
--- lib/classes/dentalClaim.php
+++ lib/classes/dentalClaim.php
@@ -2,19 +2,136 @@
 class dentalClaim{
 
 	private $claimIndex;
-
+	private $patientFirst;
+	private $patientLast;
+	private $patientMiddle;
+	private $patientId;
+	private $patientStreet;
+	private $patientCity;
+	private $patientState;
+	private $patientZip;
+	private $patientBirth;
+	private $patientSex;
+	private $payerName;
+	private $payerId;
+	private $payer2ndId;
+	private $payerStreet1;
+	private $payerStreet2;
+	private $payerCity;
+	private $payerState;
+	private $payerZip;
+	private $billingProvider2ndId;
+	private $claimId;
+	private $claimAmount;
+	private $facilityCode;
+	private $signature;
+	private $assignmentCode;
+	private $assignmentStatus;
+	private $release;
+	private $serviceDate;
+	private $claimNote;
 
 	function __construct(){
-		$this->claimIndex = 69;
+		$this->claimIndex = '';
+		$this->patientFirst = '';
+		$this->patientLast = '';
+		$this->patientMiddle = '';
+		$this->patientId = '';
+		$this->patientStreet = '';
+		$this->patientCity = '';
+		$this->patientState = '';
+		$this->patientZip = '';
+		$this->patientBirth = '';
+		$this->patientSex = '';
+		$this->payerName = '';
+		$this->payerId = '';
+		$this->payer2ndId = '';
+		$this->payerStreet1 = '';
+		$this->payerStreet2 = '';
+		$this->payerCity = '';
+		$this->payerState = '';
+		$this->payerZip = '';
+		$this->billingProvider2ndId = '';
+		$this->claimId = '';
+		$this->claimId2 = '';
+		$this->claimAmount = '';
+		$this->facilityCode = '';
+		$this->signature = '';
+		$this->assignmentCode = '';
+		$this->assignmentStatus = '';
+		$this->release = '';
+		$this->serviceDate = '';
+		$this->claimNote = '';
 	}
 
 	# SETTERS
 	function setClaimIndex($x){ $this->claimIndex = $x; }
+	function setPatientFirst($x){ $this->patientFirst = $x; }
+	function setPatientLast($x){ $this->patientLast = $x; }
+	function setPatientMiddle($x){ $this->patientMiddle = $x; }
+	function setPatientId($x){ $this->patientId = $x; }
+	function setPatientStreet($x){ $this->patientStreet = $x; }
+	function setPatientCity($x){ $this->patientCity = $x; }
+	function setPatientState($x){ $this->patientState = $x; }
+	function setPatientZip($x){ $this->patientZip = $x; }
+	function setPatientBirth($x){ $this->patientBirth = $x; }
+	function setPatientSex($x){ $this->patientSex = $x; }
+	function setPayerName($x){ $this->payerName = $x; }
+	function setPayerId($x){ $this->payerId = $x; }
+	function setPayer2ndId($x){ $this->payer2ndId = $x; }
+	function setPayerStreet1($x){ $this->payerStreet1 = $x; }
+	function setPayerStreet2($x){ $this->payerStreet2 = $x; }
+	function setPayerCity($x){ $this->payerCity = $x; }
+	function setPayerState($x){ $this->payerState = $x; }
+	function setPayerZip($x){ $this->payerZip = $x; }
+	function setBillingProvider2ndId($x){ $this->billingProvider2ndId = $x; }
+	function setClaimId($x){ $this->claimId = $x; }
+	function setClaimId2($x){ $this->claimId2 = $x; }
+	function setClaimAmount($x){ $this->claimAmount = $x; }
+	function setFacilityCode($x){ $this->facilityCode = $x; }
+	function setSignature($x){ $this->signature = $x; }
+	function setAssignmentCode($x){ $this->assignmentCode = $x; }
+	function setAssignmentStatus($x){ $this->assignmentStatus = $x; }
+	function setRelease($x){ $this->release = $x; }
+	function setServiceDate($x){ $this->serviceDate = $x; }
+	function setClaimNote($x){ $this->claimNote = $x; }
 
 	function toText(){
 		#to text by defaul create a string ment to be viewed by html
 		$m = array();
 		$m[] = "ClaimIndex: ".$this->claimIndex;
+		$m[] = "ClaimNote: ".$this->claimNote;
+/*
+		$m[] = "ClaimId: ".$this->claimId;
+		$m[] = "ClaimId2: ".$this->claimId2;
+		$m[] = "ServiceDate: ".$this->serviceDate;
+		$m[] = "PatientFirst: ".$this->patientFirst;
+		$m[] = "PatientLast: ".$this->patientLast;
+		$m[] = "PatientMiddle: ".$this->patientMiddle;
+		$m[] = "PatientId: ".$this->patientId;
+		$m[] = "PatientStreet: ".$this->patientStreet;
+		$m[] = "PatientCity: ".$this->patientCity;
+		$m[] = "PatientState: ".$this->patientState;
+		$m[] = "PatientZip: ".$this->patientZip;
+		$m[] = "PatientBirth: ".$this->patientBirth;
+		$m[] = "PatientSex: ".$this->patientSex;
+		$m[] = "PayerName: ".$this->payerName;
+		$m[] = "PayerId: ".$this->payerId;
+		$m[] = "Payer2ndId: ".$this->payer2ndId;
+		$m[] = "PayerStreet1: ".$this->payerStreet1;
+		$m[] = "PayerStreet2: ".$this->payerStreet2;
+		$m[] = "PayerCity: ".$this->payerCity;
+		$m[] = "PayerState: ".$this->payerState;
+		$m[] = "PayerZip: ".$this->payerZip;
+		$m[] = "BillingProvider2ndId: ".$this->billingProvider2ndId;
+		$m[] = "ClaimAmount: ".$this->claimAmount;
+		$m[] = "FacilityCode: ".$this->facilityCode;
+		$m[] = "Signature: ".$this->signature;
+		$m[] = "AssignmentCode: ".$this->assignmentCode;
+		$m[] = "AssignmentStatus: ".$this->assignmentStatus;
+		$m[] = "Release: ".$this->release;
+*/
+
 		$r = implode('<br/>',$m);
 		return $r;
 	}
