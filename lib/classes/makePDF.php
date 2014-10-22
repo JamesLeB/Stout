cb82ce69600aa28dd07cae2d955c1be604bf0d34
JamesLeB <james@gowanustech.com>
2014-10-22 17:09:59 -0400

    log entry

--- lib/classes/makePDF.php
+++ lib/classes/makePDF.php
@@ -2,8 +2,8 @@
 class makePDF {
 	private $sceneCount = 3;
 	private $model;
-	private $index    = 300001;
-	private $maxIndex = 500000;
+	private $index    = 500001;
+	private $maxIndex = 656824;
 
 	function __construct($model){
 		$this->model = $model;
