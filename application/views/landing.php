2a38fa0f8f2ac7b5065688ad18dd6c36e325ed23
JamesLeB <james@gowanustech.com>
2014-11-21 17:30:23 -0500

    correct problems with landing

--- application/views/landing.php
+++ application/views/landing.php
@@ -17,55 +17,44 @@
 		});
 	</script>
 	<style>
+/*
 		div{
 			border : dashed 1px gray;
 			margin : 5px;
 		}
+*/
+		#wrapper{
+			border : 1px solid green;
+			background : white;
+			padding : 5px;
+		}
+		#wrapper div:nth-child(1) > img{
+			height :50px;
+		}
+		#logout{
+			float : right;
+			margin-right : 20px;
+		}
 		#logout img{
 			height : 40px;
 		}
-		.slide{
+		#screen{
 			position : fixed;
-			margin : 0;
-			left : 1%;
-			top : 1%;
-			height : 97%;
-			width  : 97%;
-			opacity : .5;
-		}
-		#slide1{
+			margin : 0px;
+			top : 0;
+			left : 0;
+			width : 100%;
+			height : 100%;
+			z-index : -1;
 			background : gray;
-			z-index : 1;
-		}
-		#slide2{
-			background : gray;
-			z-index : 2;
-		}
-		#slide3{
-			background : white;
-			z-index : 3;
-			opacity : 1;
-		}
-		#slide3 > div:nth-child(1){
-			height : 50px;
-		}
-		#slide3 > div:nth-child(2){
-			height : 400px;
-		}
-		#slide3 > div:nth-child(3){
-			height : 50px;
-		}
-		#slide3 button{
-			float : right;
 		}
 	</style>
 </head>
 <body>
-	<div id='slide1' class='slide'></div>
-	<div id='slide2' class='slide'></div>
-	<div id='slide3' class='slide'>
+	<div id='screen'></div>
+	<div id='wrapper'>
 		<div>
-			<span>Heading</span>
+			<img src='lib/images/cricket.png'>
 			<button id='logout'><img src='lib/images/exit.png'></button>
 		</div>
 		<div>
@@ -75,7 +64,7 @@
 					<li><a href='#tab2'>Claims</a></li>
 				</ul>
 				<div id='tab1'>
-					<span>Batches</span>
+					<?php echo $jtable ?>
 				</div>
 				<div id='tab2'>
 					<span>Claims</span>
