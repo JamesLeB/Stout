55aaf1dab68e9ca2b3ba18101e52b4088bf39546
JamesLeB <james@gowanustech.com>
2014-11-22 15:23:00 -0500

    add character table load button

--- application/views/classes/jCharacter.php
+++ application/views/classes/jCharacter.php
@@ -1,23 +1,33 @@
 <div id='characterSheet'>
-	<div id='charTable'><?php echo $charTable; ?></div>
+	<div id='charTable'>
+		<div><button>Load</button></div>
+		<div><?php echo $charTable; ?></div>
+	</div>
 	<div id='charSheet'><?php echo $charSheet; ?></div>
 </div>
 <style>
 	#characterSheet{
-		border : 1px dashed red;
 	}
 	#charTable{
-		border : 1px solid green;
 		width : 650px;
 		padding : 10px;
 		float : right;
 	}
+	#charTable > div:nth-child(1){
+		margin-bottom : 10px;
+		width : 80%;
+		margin-left : auto;
+		margin-right : auto;
+	}
 	#charSheet{
 		width : 500px;
-		height : 500px;
-		background : lightgreen;
+		height : 450px;
 	}
 </style>
 <script>
-	$('#charTable').hide();
+	$('#charTable > div:nth-child(1) > button').click(function(){
+		$.post('index.php?/classes/characterSheet/load','',function(data){
+			$('#charTable > div:nth-child(2)').html(data);
+		});
+	});
 </script>
