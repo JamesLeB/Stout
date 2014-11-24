a8d4a7e3a09b839307276cd593ad08c76b23fdf4
JamesLeB <james@gowanustech.com>
2014-11-23 21:36:16 -0500

    add functions to alter db

--- application/models/local.php
+++ application/models/local.php
@@ -16,5 +16,43 @@ class Local extends CI_Model{
 		}
 		return $rtn;
 	}
+	function getTableStatus(){
+		$tables = array();
+		$table = array(
+			'name' => 'Characters',
+			'status' => 'Offline'
+		);
+		$tables[] = $table;
+		$table = array(
+			'name' => 'Inventory',
+			'status' => 'Offline'
+		);
+		$tables[] = $table;
+		return $tables;
+	}
+	function createTable($t){
+		$m = array();
+		$m[] = "Creating table: $t";
+		if($t == 'Characters'){
+			$m[] = "Loading Query to create Character Table";
+			$query = "Create table Characters (id int)";
+			$m[] = "Dropping Table";
+			$rs = $this->db->query($query);
+			$m[] = $rs ? 'TRUE' : 'FALSE';
+		}
+		return implode('<br/>',$m);
+	}
+	function deleteTable($t){
+		$m = array();
+		$m[] = "Deleting table: $t";
+		if($t == 'Characters'){
+			$m[] = "Loading Query to delete Character Table";
+			$query = "drop table Characters";
+			$m[] = "Dropping table";
+			$rs = $this->db->query($query);
+			$m[] = $rs ? 'TRUE' : 'FALSE';
+		}
+		return implode('<br/>',$m);
+	}
 }
 ?>
