6b59edcbe06b152c088b8f05a5f7ee5e1cd0b93c
JamesLeB <james@gowanustech.com>
2014-12-05 10:55:18 -0500

    build coin list

--- application/views/classes/coins.php
+++ application/views/classes/coins.php
@@ -1,1 +1,51 @@
-hello from the bitcoin world :)
+<?php
+	$coinList = '';
+	foreach($list as $coin){
+		$coinList .= "
+			<div>
+				<div>$coin</div>
+			</div>";
+	}
+	$html = "
+		<div id='coinList'>
+			<div>Coin List</div>
+			<div>$coinList</div>
+		</div>
+	";
+	$style = "
+		<style>
+			#coinList{
+				border : 1px solid gray;
+				box-shadow : 3px 3px 3px;
+				padding : 20px;
+				border-radius : 20px;
+				width : 800px;
+				margin : 20px;
+				margin-left : auto;
+				margin-right : auto;
+				background : #F0F0F0;
+			}
+			#coinList div{
+				margin : 10px;
+			}
+			#coinList > div:nth-child(1){
+				padding-left : 50px;
+				font-size : 120%;
+			}
+			#coinList > div:nth-child(2) > div{
+				border : 1px solid gray;
+				box-shadow : 2px 2px 2px;
+				border-radius : 20px;
+				margin-bottom : 20px;
+				background : white;
+			}
+			#coinList > div:nth-child(2) > div > div{
+				width : 200px;
+				margin-left : 20px;
+			}
+		</style>
+	";
+	
+	echo $style;
+	echo $html;
+?>
