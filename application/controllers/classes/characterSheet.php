c1f60bde5647e162c82b852bcf29c3d2f06ea2ad
JamesLeB <james@gowanustech.com>
2014-12-09 11:47:47 -0500

    clean up new character code

--- application/controllers/classes/characterSheet.php
+++ application/controllers/classes/characterSheet.php
@@ -4,6 +4,7 @@ class CharacterSheet extends CI_Controller {
 
 	public function index()
 	{
+/*
 		$name  = $_POST['charName'];
 		$race  = $_POST['charRace'];
 		$class = $_POST['charClass'];
@@ -12,10 +13,13 @@ class CharacterSheet extends CI_Controller {
 		$rtn .= "Race $race<br/>";
 		$rtn .= "Class $class<br/>";
 		$rtn .= "";
-
 		echo $rtn;
+*/
+		echo "index";
 	}
-	public function load(){
+	public function loadCharacterTable(){
+		echo "load character sheet";
+/*
 		# SETUP Char table
 		$heading = array('ID','Name','Race','Class');
 		$rows = array();
@@ -34,31 +38,7 @@ class CharacterSheet extends CI_Controller {
 		# END SETUP Char table
 		$rtn = $this->load->view('classes/jtable',$charTable,true);
 		echo $rtn;
-	}
-	public function create(){
-		$table  = $_POST['table'];
-		$action = $_POST['action'];
-
-		$this->load->model('local');
-		$rtn = 'ss';
-		if($action == 'Create'){
-			$rtn = $this->local->createTable($table);
-		}elseif($action == 'Delete'){
-			$rtn = $this->local->deleteTable($table);
-		}else{
-			$rtn = "Bad command";
-		}
-
-		echo "Comand: $action $table<br/>From model: $rtn";
-	}
-	public function check(){
-		$this->load->model('local');
-		$tables = $this->local->getTableStatus();
-
-		$d['objName'] = 'databaseTables';
-		$d['tables'] = $tables;
-		$data = $this->load->view('classes/localdb',$d,true);
-		echo "$data";
+*/
 	}
 	public function test(){
 		echo "test function";
--- application/views/classes/jCharacter.php
+++ application/views/classes/jCharacter.php
@@ -1,9 +1,9 @@
 <div id='newCharacterSheet'>
 	<div id='charTable'>
 		<div>
+			<button>Go</button>
 			<button>Load</button>
-			<button>Create</button>
-			<button>Check</button>
+			<button>Test</button>
 		</div>
 		<div><?php echo $charTable; ?></div>
 	</div>
@@ -11,19 +11,25 @@
 </div>
 <style>
 	#newCharacterSheet{
-		border : 1px black solid;
+		border : 1px solid gray;
+		border-radius : 20px;
+		box-shadow : 2px 2px 2px black;
 		padding : 10px;
 		height : 300px;
 	}
 	#charTable{
 		border : 1px solid blue;
+		margin-left : 30px;
+		margin-top : 30px;
 		width : 650px;
 		padding : 10px;
 	}
-	#charTable div{
-		border : 1px dashed gray;
+	#charTable > div:nth-child(1) > button{
+		margin-bottom : 10px;
+		margin-left : 20px;
 	}
-	#charTable > div:nth-child(1){
+	#charTable > div:nth-child(2){
+		border : 1px dashed gray;
 	}
 	#charSheet{
 		width : 500px;
@@ -33,23 +39,16 @@
 <script>
 	$('#charSheet').hide();
 
-	// Code to trigger a button durning testingg
-/*
-	$.post('index.php?/classes/characterSheet/check','',function(data){
-			$('#charTable > div:nth-child(2)').html(data);
-	});
-*/
-
 	$('#charTable > div:nth-child(1) > button').click(function(){
 		var target = 'index.php?/classes/characterSheet/';
 		var func = '';
 		var a = $(this).first().html();
-		     if(a=='Load')  {func = 'load';}
-		else if(a=='Create'){func = 'create';}
-		else if(a=='Check') {func = 'check';}
-		else                {func = 'test';}
+		     if(a=='Go')    {func = 'index';}
+		else if(a=='Load')  {func = 'loadCharacterTable';}
+		else if(a=='Test')  {func = 'test';}
 		$.post(target+func,'',function(data){
-			$('#charTable > div:nth-child(2)').html(func);
+			$('#charTable > div:nth-child(2)').html(data);
+			//$('#error').html(func);
 		});
 	});
 </script>
