6352f3fd64902eade7eedd02a0d6e770c45ce55a
JamesLeB <james@gowanustech.com>
2014-12-11 18:54:31 -0500

    working on splash page

--- application/views/slides/splash.php
+++ application/views/slides/splash.php
@@ -1,1 +1,34 @@
-hello from splash
+<div id='splash'>
+		<button>GO</button>
+		<textarea></textarea>
+</div>
+<style>
+	#splash{
+		background : gray;
+		height : 400px;
+		width :  800px;
+		margin-left  : auto;
+		margin-right : auto;
+		position : relative;
+	}
+	#splash textarea{
+		resize : none;
+		height : 300px;
+		width  : 600px;
+		position : absolute;
+		left : 100px;
+		top : 20px;
+		padding : 10px;
+	}
+	#splash button{
+		position : absolute;
+		left : 20px;
+		top : 20px;
+	}
+</style>
+<script>
+	$('#splash button').click(function(){
+alert('ya');
+		$('#splash textarea').html('input here \nnext');
+	});
+</script>
