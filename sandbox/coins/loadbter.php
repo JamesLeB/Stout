75fda169fb3d8e497ac5d29a57e0824b23989849
JamesLeB <james@gowanustech.com>
2014-12-12 13:00:36 -0500

    get trading pairs from bter

--- sandbox/coins/loadbter.php
+++ sandbox/coins/loadbter.php
@@ -1,6 +1,14 @@
 <?php
+	$json = file_get_contents('http://data.bter.com/api/1/pairs');
+	$list = json_decode($json,true);
 	$rtn = array();
 	$rtn[] = "First...Get list of trading pairs";
+	$count = 0;
+	foreach($list as $l){
+		$count++;
+		$rtn[] = "\t$l";
+	}
+	$rtn[] = "\t$count pairs";
 	$rtn[] = "Second...Run list and get history";
 	$rtn[] = "Third...Load trades into DB";
 	$rtn[] = "Fourth...Create final report";
