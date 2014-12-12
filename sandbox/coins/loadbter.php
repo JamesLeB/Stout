cd0eadbaf05ba1cd6eab1d9e611f033c17331dc0
JamesLeB <james@gowanustech.com>
2014-12-12 12:17:29 -0500

    start work on bter data loader

--- sandbox/coins/loadbter.php
+++ sandbox/coins/loadbter.php
@@ -1,1 +1,8 @@
-loading bter
+<?php
+	$rtn = array();
+	$rtn[] = "First...Get list of trading pairs";
+	$rtn[] = "Second...Run list and get history";
+	$rtn[] = "Third...Load trades into DB";
+	$rtn[] = "Fourth...Create final report";
+	echo implode("\n",$rtn);
+?>
