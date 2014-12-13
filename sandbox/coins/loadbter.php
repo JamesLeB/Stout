3919f01421ef2933ecf4ba997aca07e83af4c298
JamesLeB <james@gowanustech.com>
2014-12-13 17:12:16 -0500

    switch loadbter to use new bter object

--- sandbox/coins/loadbter.php
+++ sandbox/coins/loadbter.php
@@ -1,20 +1,36 @@
 <?php
+	$startTime = time();
+	include("bter.php");
 	$rtn = array();
-	#$json = file_get_contents('http://data.bter.com/api/1/pairs');
-	#$list = json_decode($json,true);
+	$bter = new bter();
+	$json = file_get_contents('http://data.bter.com/api/1/pairs');
+	$list = json_decode($json,true);
 	$rtn[] = "First...Get list of trading pairs";
-	#$rtn[] = $list[0];
-	$rtn[] = "\tbtc_cny";
-	$count = 0;
-	if(0){
-		foreach($list as $l){
-			$count++;
-			$rtn[] = "\t$l";
-		}
+	$pairs = count($list);
+	$rtn[] = "\t$pairs pairs";
+
+	$rtn[] = "Second...Load trades into DB";
+	$obj = $bter->uploadBterData('btc_cny');
+	$count   = 0;
+	$records = 0;
+	$errors  = 0;
+	$records += $obj['records'];
+	$errors  += $obj['errors'];
+/*
+	foreach($list as $l){
+		$count++;
+		$rtn[] = "\t$l";
 	}
-	$rtn[] = "\t$count pairs";
-	$rtn[] = "Second...Run list and get history";
-	$rtn[] = "Third...Load trades into DB";
-	$rtn[] = "Fourth...Create final report";
+*/
+	$rtn[] = "\t$count pairs loaded";
+
+	$rtn[] = "Third...Create final report";
+	$endTime = time();
+	$st = date('Y-m-d H:i:s',$startTime);
+	$et = date('Y-m-d H:i:s',$endTime);
+	$rtn[] = "\tS Time : $st";
+	$rtn[] = "\tE Time : $et";
+	$rtn[] = "\tRecords: $records";
+	$rtn[] = "\tErrors: $errors";
 	echo implode("\n",$rtn);
 ?>
