565f701970b994c0a7b82254db3b3a0f5c4f32f0
JamesLeB <james@gowanustech.com>
2014-12-15 11:41:27 -0500

    minor format changes

--- application/views/junior/juniorHome.php
+++ application/views/junior/juniorHome.php
@@ -207,18 +207,24 @@
 		position : relative;
 	}
 	#juniorBiller > div{
-		width : 250px;
 		position : absolute;
 	}
-	#juniorBiller > div:nth-child(1){ top : 20px; left :  50px;}
-	#juniorBiller > div:nth-child(2){ top : 20px; left : 350px;}
-	#juniorBiller > div:nth-child(3){ top : 20px; left : 650px;}
-	#juniorBiller > div:nth-child(4){ top : 20px; left : 950px;}
+	#juniorBiller > div:nth-child(1){ width : 250px; top : 20px; left :  50px;}
+	#juniorBiller > div:nth-child(2){ width : 250px; top : 20px; left : 350px;}
+	#juniorBiller > div:nth-child(3){ width : 250px; top : 20px; left : 650px;}
+	#juniorBiller > div:nth-child(4){ width : 250px; top : 20px; left : 950px;}
 	#juniorBiller > div > div:nth-child(1){
 		font-size : 120%;
 		width : 300px;
 		margin-left : 20px;
 		margin-bottom : 5px;
+	}
+	#debug{
+		height : 100px;
+		width : 400px;
+		border : 1px solid green;
+		top : 400px;
+		left : 100px;
 	}
 </style>
 <!-- End Dev CSS --!>
@@ -241,20 +247,25 @@
 							<div>To Send</div>
 							<div></div>
 						</div>
+						<div id='debug'></div>
+						<button>GO</button>
 					</div>
 <!-- End Dev Html--!>
 				</div>
 <!-- Start Dev --!>
 <script>
-	var target = 'index.php?/junior/';
-	var func = 'go';
-	$.post(target+func,'',function(data){
-		var obj = $.parseJSON(data);
-		$('#juniorBiller > div:nth-child(1) > div:nth-child(2)').html(obj[0]);
-		$('#juniorBiller > div:nth-child(2) > div:nth-child(2)').html(obj[1]);
-		$('#juniorBiller > div:nth-child(3) > div:nth-child(2)').html(obj[2]);
-		$('#juniorBiller > div:nth-child(4) > div:nth-child(2)').html(obj[3]);
+	$('#juniorBiller button').click(function(){
+		var target = 'index.php?/junior/';
+		var func = 'go';
+		$.post(target+func,'',function(data){
+			var obj = $.parseJSON(data);
+			$('#juniorBiller > div:nth-child(1) > div:nth-child(2)').html(obj[0]);
+			$('#juniorBiller > div:nth-child(2) > div:nth-child(2)').html(obj[1]);
+			$('#juniorBiller > div:nth-child(3) > div:nth-child(2)').html(obj[2]);
+			$('#juniorBiller > div:nth-child(4) > div:nth-child(2)').html(obj[3]);
+		});
 	});
+	$('#juniorBiller button').trigger('click');
 </script>
 <!-- End Dev --!>
 			</div>
