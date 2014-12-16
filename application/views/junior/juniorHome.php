a568a754a62fa8ef924af37c9ebf556ef255d408
JamesLeB <james@gowanustech.com>
2014-12-16 12:08:13 -0500

    break junior home into seperate views

--- application/views/junior/juniorHome.php
+++ application/views/junior/juniorHome.php
@@ -7,7 +7,6 @@
 	<link  href='js/jquery-ui-1.11.2.custom/jquery-ui.css' rel='stylesheet'/>
 	<script>
 		$(document).ready(function(){
-			//alert('Document Ready');
 			$('#logout').click(function(){
 				$.post('index.php?/stout/logout','',function(data){
 					window.location.href = "index.php";
@@ -15,60 +14,9 @@
 			});
 			$('#tabs').tabs();
 			$('#tabs').tabs({active:2});
-			$('#juniorTasks > div:nth-child(1) > button').click(function(){
-				var action = $(this).first().html();
-				if(action == 'Clear'){
-					$('#juniorTasks > div:nth-child(2) > div').html('');
-					$('#juniorTasks > div:nth-child(3) > div').html('');
-				}else if(action == 'Two'){
-					$('#juniorTasks').css('background','lightgray');
-					var target = 'index.php?/junior/';
-					var func = 'getFtpBatchList';
-					var request = $.post(target+func,parm,function(data){
-						var obj = $.parseJSON(data);
-						var html = '<div>';
-						obj.forEach(function(e){
-							var test = e.match(/\./);
-							if(test){html += '<div>'+e+'</div>';}
-						});
-						html += '</div>';
-						$('#juniorTasks').css('background','white');
-						$('#juniorTasks > div:nth-child(3) > div').html(html);
-						$('#juniorTasks > div:nth-child(3) > div > div > div').unbind('mouseover');
-						$('#juniorTasks > div:nth-child(3) > div > div > div').unbind('mouseout');
-						$('#juniorTasks > div:nth-child(3) > div > div > div').unbind('click');
-						$('#juniorTasks > div:nth-child(3) > div > div > div').mouseover(function(){
-							$(this).css({'background':'blue','color':'white'});
-						});
-						$('#juniorTasks > div:nth-child(3) > div > div > div').mouseout(function(){
-							$(this).css({'background':'white','color':'black'});
-						});
-						$('#juniorTasks > div:nth-child(3) > div > div > div').click(function(){
-							alert('here we go');
-						});
-					});
-				}else{
-					$('#juniorTasks').css('background','lightgray');
-					var target = 'index.php?/junior/';
-					var func = 'action';
-					var parm = {message: action};
-					var request = $.post(target+func,parm,function(data){
-						$('#juniorTasks').css('background','white');
-						$('#juniorTasks > div:nth-child(2)').html(data);
-					});
-				}
-			});// END Button click function
-			$('#juniorTasks > div:nth-child(1) > button:nth-child(2)').trigger('click');
-			$('#juniorTasks > div:nth-child(2)').hide();
-		});
+		}); // END doc ready function
 	</script>
 	<style>
-/*
-		div{
-			border : dashed 1px gray;
-			margin : 5px;
-		}
-*/
 		#wrapper{
 			border : 1px solid green;
 			background : white;
@@ -98,75 +46,17 @@
 			z-index : -1;
 			background : gray;
 		}
-		#juniorTasks{
-			border : 1px solid gray;
-			border-radius : 20px;
-			box-shadow : 3px 3px 3px black;
-			height : 500px;
-			margin : 20px;
-		}
-		#juniorTasks div{
-			/*
-			border : 1px gray dashed;
-			*/
-		}
-		#juniorTasks > div{
-			height : 300px;
-			float : left;
-			margin-top : 40px;
-		}
-		#juniorTasks > div:nth-child(1){
-			width : 120px;
-			margin-left : 50px;
-		}
-		#juniorTasks > div:nth-child(1) > button{
-			width : 100px;
-			margin-bottom : 15px;
-			margin-left : 10px;
-		}
-		#juniorTasks > div:nth-child(1) > button:nth-child(1){
-			margin-top : 30px;
-		}
-		#juniorTasks > div:nth-child(2) > span{
-			padding-left : 60px;
-		}
-		#juniorTasks > div:nth-child(2) > div{
-			width : 400px;
-			height : 200px;
-			margin-left : 10px;
-			border : 2px inset gray;
-			background : white;
-			padding : 10px;
-			overflow : auto;
-		}
-		#juniorTasks > div:nth-child(3) > span{
-			padding-left : 60px;
-		}
-		#juniorTasks > div:nth-child(3) > div{
-			width : 300px;
-			height : 200px;
-			margin-left : 20px;
-			border : 2px inset gray;
-			background : white;
-			padding : 10px;
-		}
-		#juniorTasks > div:nth-child(3) > div > div > div{
-			width : 200px;
-			margin-bottom : 5px;
-			padding : 2px;
-		}
-		li{
-			margin-top : 10px;
-		}
 	</style>
 </head>
 <body>
 	<div id='screen'></div>
 	<div id='wrapper'>
+		<!-- Header -->
 		<div>
 			<img src='lib/images/nyu.jpg'>
 			<button id='logout'><img src='lib/images/exit.png'></button>
 		</div>
+		<!-- Body -->
 		<div>
 			<div id='tabs'>
 				<ul>
@@ -174,112 +64,12 @@
 					<li><a href='#tab2'>ToDo</a></li>
 					<li><a href='#tab3'>Bill Medicaid</a></li>
 				</ul>
-				<div id='tab1'>
-<!-- START Junior page 1 -->
-					<div id='juniorTasks'>
-						<div>
-							<button>One</button>
-							<button>Two</button>
-							<button>Clear</button>
-						</div>
-						<div><span>Name</span><div></div></div>
-						<div><span>File List</span><div></div></div>
-					</div>
-<!-- END Junior page 1 -->
-				</div>
-				<div id='tab2'>
-					<h3>Stuff to do</h3>
-					<ul>
-						<li>Move axium batch sheet to google drive</li>
-						<li>Develop function to move selected file</li>
-						<li>Develop function to convert select file</li>
-						<li>Develop function to move file to ftp server</li>
-						<li></li>
-					</ul>
-				</div>
-<!-- Start Dev CSS --!>
-<style>
-	#juniorBiller{
-		background : lightgray;
-		border-radius : 20px;
-		box-shadow : 3px 3px 3px black;
-		height : 650px;
-		position : relative;
-	}
-	#juniorBiller > div{
-		position : absolute;
-	}
-	#juniorBiller > div:nth-child(1){ width : 250px; top :  20px; left :  50px;}
-	#juniorBiller > div:nth-child(2){ width : 250px; top :  20px; left : 350px;}
-	#juniorBiller > div:nth-child(3){ width : 250px; top : 320px; left :  50px;}
-	#juniorBiller > div:nth-child(4){ width : 250px; top :  20px; left : 650px;}
-	#juniorBiller > div:nth-child(5){ width : 250px; top :  20px; left : 950px;}
-	#juniorBiller > div > div:nth-child(1){
-		font-size : 120%;
-		width : 300px;
-		margin-left : 20px;
-		margin-bottom : 5px;
-	}
-	#debug{
-		padding : 20px;
-		height : 250px;
-		width : 800px;
-		border : 1px inset green;
-		top : 320px;
-		left : 400px;
-		background : white;
-		overflow : auto;
-	}
-</style>
-<!-- End Dev CSS --!>
-				<div id='tab3'>
-<!-- Start Dev Html--!>
-					<div id='juniorBiller'>
-						<div>
-							<div>Batches</div>
-							<div></div>
-						</div>
-						<div>
-							<div>To Process</div>
-							<div></div>
-						</div>
-						<div>
-							<div>Processed</div>
-							<div></div>
-						</div>
-						<div>
-							<div>To Transfer</div>
-							<div></div>
-						</div>
-						<div>
-							<div>To Send</div>
-							<div></div>
-						</div>
-						<div id='debug'></div>
-						<button>GO</button>
-					</div>
-<!-- End Dev Html--!>
-				</div>
-<!-- Start Dev --!>
-<script>
-	$('#juniorBiller button').hide();
-	$('#juniorBiller button').click(function(){
-		var target = 'index.php?/junior/';
-		var func = 'go';
-		$.post(target+func,'',function(data){
-			var obj = $.parseJSON(data);
-			$('#juniorBiller > div:nth-child(1) > div:nth-child(2)').html(obj[0]);
-			$('#juniorBiller > div:nth-child(2) > div:nth-child(2)').html(obj[1]);
-			$('#juniorBiller > div:nth-child(3) > div:nth-child(2)').html(obj[2]);
-			$('#juniorBiller > div:nth-child(4) > div:nth-child(2)').html(obj[3]);
-			$('#juniorBiller > div:nth-child(5) > div:nth-child(2)').html(obj[4]);
-		});
-	});
-	$('#juniorBiller button').trigger('click');
-</script>
-<!-- End Dev --!>
+				<div id='tab1'><!-- Junior Tasks --></div>
+				<div id='tab2'><!-- To Do --></div>
+				<div id='tab3'><!-- Bill Medicaid --></div>
 			</div>
 		</div>
+		<!-- Footer -->
 		<div>
 			<span>Footer</span>
 		</div>
