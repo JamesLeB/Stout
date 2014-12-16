58b06580793dc1d50511284148b9764b3ce000bd
JamesLeB <james@gowanustech.com>
2014-12-16 09:56:57 -0500

    format transfer folder

--- application/views/junior/juniorHome.php
+++ application/views/junior/juniorHome.php
@@ -209,10 +209,11 @@
 	#juniorBiller > div{
 		position : absolute;
 	}
-	#juniorBiller > div:nth-child(1){ width : 250px; top : 20px; left :  50px;}
-	#juniorBiller > div:nth-child(2){ width : 250px; top : 20px; left : 350px;}
-	#juniorBiller > div:nth-child(3){ width : 250px; top : 20px; left : 650px;}
-	#juniorBiller > div:nth-child(4){ width : 250px; top : 20px; left : 950px;}
+	#juniorBiller > div:nth-child(1){ width : 250px; top :  20px; left :  50px;}
+	#juniorBiller > div:nth-child(2){ width : 250px; top :  20px; left : 350px;}
+	#juniorBiller > div:nth-child(3){ width : 250px; top : 320px; left :  50px;}
+	#juniorBiller > div:nth-child(4){ width : 250px; top :  20px; left : 650px;}
+	#juniorBiller > div:nth-child(5){ width : 250px; top :  20px; left : 950px;}
 	#juniorBiller > div > div:nth-child(1){
 		font-size : 120%;
 		width : 300px;
@@ -224,8 +225,8 @@
 		height : 250px;
 		width : 800px;
 		border : 1px inset green;
-		top : 300px;
-		left : 100px;
+		top : 320px;
+		left : 400px;
 		background : white;
 		overflow : auto;
 	}
@@ -235,7 +236,7 @@
 <!-- Start Dev Html--!>
 					<div id='juniorBiller'>
 						<div>
-							<div>Batch</div>
+							<div>Batches</div>
 							<div></div>
 						</div>
 						<div>
@@ -244,6 +245,10 @@
 						</div>
 						<div>
 							<div>Processed</div>
+							<div></div>
+						</div>
+						<div>
+							<div>To Transfer</div>
 							<div></div>
 						</div>
 						<div>
@@ -266,6 +271,7 @@
 			$('#juniorBiller > div:nth-child(2) > div:nth-child(2)').html(obj[1]);
 			$('#juniorBiller > div:nth-child(3) > div:nth-child(2)').html(obj[2]);
 			$('#juniorBiller > div:nth-child(4) > div:nth-child(2)').html(obj[3]);
+			$('#juniorBiller > div:nth-child(5) > div:nth-child(2)').html(obj[4]);
 		});
 	});
 	$('#juniorBiller button').trigger('click');
