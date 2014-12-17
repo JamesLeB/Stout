9c3d6995b03c48c1f584ef0ccabc7f9896c0ef5d
JamesLeB <james@gowanustech.com>
2014-12-17 17:04:35 -0500

    add getExchanges and get history functions to model

--- application/models/exchange.php
+++ application/models/exchange.php
@@ -1,14 +1,36 @@
 <?php
 class Exchange extends CI_Model{
 
-	#private $db;
+	private $db;
 
 	function __construct(){
 		parent::__construct();
-		#$this->db = $this->load->database('dentrix',true);
+		$this->db = $this->load->database('local',true);
 	}
 	public function index(){
 		return "message from exchange model";
+	}
+	public function getExchanges(){
+		$pairs = array();
+		$pairs[] = array('name'=>'btc_uds');
+		$pairs[] = array('name'=>'btc_cyn');
+		$exchange = array(
+			'name' => 'Bter',
+			'pairs' => $pairs
+		);
+		return array($exchange);
+	}
+	public function getHistory(){
+		$q = 'select pair,time_stamp,price,amount,tid,type,date from bter limit 100';
+		$rs = $this->db->query($q);
+		$rows = array();
+		$ct = 0;
+		foreach($rs->result_array() as $row)
+		{
+			$ct++;
+			$rows[] = $row;
+		}
+		return $rows;
 	}
 }
 ?>
