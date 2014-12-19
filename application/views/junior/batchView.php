6b596f512e5b58a0f7a92c3df62056c1701b11bd
JamesLeB <james@gowanustech.com>
2014-12-19 13:03:44 -0500

    adjust css

--- application/views/junior/batchView.php
+++ application/views/junior/batchView.php
@@ -2,15 +2,17 @@
 	<div>
 		<?php echo $batch ?>
 	</div>
+	<div>
+		<?php echo $batchData ?>
+	</div>
 </div>
 <style>
 	#batchView
 	{
 		background : white;
-		height : 500px;
-		width  : 600px;
+		width  : 1200px;
 		opacity : 1;
-		margin-top   : 50px;;
+		margin-top   : 20px;;
 		margin-left  : auto;
 		margin-right : auto;
 		border : solid 1px gray;
@@ -21,5 +23,17 @@
 	{
 		margin-top  : 30px;
 		margin-left : 30px;
+	}
+	#batchView > div:nth-child(2)
+	{
+		border : 2px inset gray;
+		overflow : auto;
+		height : 450px;
+		width : 85%;
+		margin-bottom : 40px;
+		margin-top    : 20px;;
+		margin-left   : auto;
+		margin-right  : auto;
+		padding : 20px;
 	}
 </style>
