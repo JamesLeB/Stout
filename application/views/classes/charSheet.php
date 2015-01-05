f2619b6686667271d80bfe433c1d3a8344950c0b
JamesLeB <james@gowanustech.com>
2015-01-04 21:04:09 -0500

    working on new character sheet

--- application/views/classes/charSheet.php
+++ application/views/classes/charSheet.php
@@ -1,3 +1,27 @@
+<?php
+	$str = array('Str','18','+4');
+	$dex = array('Dex','18','+4');
+	$con = array('Con','18','+4');
+	$int = array('Int','18','+4');
+	$wis = array('Wis','18','+4');
+	$cha = array('Cha','18','+4');
+	$a = array(
+		$str,
+		$dex,
+		$con,
+		$int,
+		$wis,
+		$cha
+	);
+	$attributes = "";
+	foreach($a as $b){
+		$attributes .= "<div>";
+		foreach($b as $c){
+			$attributes .= "<div>$c</div>";
+		}
+		$attributes .= "</div>";
+	}
+?>
 <div id='sheet'>
 	<div>
 		<div>Name</div>
@@ -5,24 +29,36 @@
 		<div>Class</div>
 	</div>
 	<div>
+		<div>XP</div>
+		<div>HP</div>
+		<div>AC</div>
+	</div>
+	<div>
+		<?php echo $attributes ?>
+	</div>
+	<div>
+		Attacks
 		<div>
-			Str
+			<div>Weapon</div>
+			<div>+6</div>
+			<div>1d6 + 4</div>
 		</div>
-		<div>
-			Dex
-		</div>
-		<div>
-			Con
-		</div>
-		<div>
-			Int
-		</div>
-		<div>
-			Wis
-		</div>
-		<div>
-			Cha
-		</div>
+	</div>
+	<div>
+		Skills
+	</div>
+	<div>
+		Equipment
+	</div>
+	<div>
+		Magic
+		<div>Spell Attack</div>
+		<div>Spell Save</div>
+		<div>Spell Slots</div>
+		Spells
+	</div>
+	<div>
+		Damage
 	</div>
 </div>
 <style>
@@ -49,6 +85,43 @@
 	}
 	#sheet > div:nth-child(2)
 	{
-		width : 300px;
+		width : 250px;
+	}
+	#sheet > div:nth-child(3)
+	{
+		width : 250px;
+	}
+	#sheet > div:nth-child(3) > div
+	{
+		height : 35px;
+	}
+	#sheet > div:nth-child(3) > div > div
+	{
+		width : 50px;
+		float : left;
+	}
+	#sheet > div:nth-child(4)
+	{
+		width : 450px;
+	}
+	#sheet > div:nth-child(4) > div
+	{
+		height : 35px;
+	}
+	#sheet > div:nth-child(4) > div > div
+	{
+		float : left;
+	}
+	#sheet > div:nth-child(4) > div > div:nth-child(1)
+	{
+		width : 150px;
+	}
+	#sheet > div:nth-child(4) > div > div:nth-child(2)
+	{
+		width : 50px;
+	}
+	#sheet > div:nth-child(4) > div > div:nth-child(3)
+	{
+		width : 120px;
 	}
 </style>
