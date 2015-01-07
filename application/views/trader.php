0a8c817bbcefdd570af30204131bc688b530ec76
JamesLeB <james@gowanustech.com>
2015-01-07 14:36:51 -0500

    add code to display data retrieved from model

--- application/views/trader.php
+++ application/views/trader.php
@@ -7,16 +7,84 @@
 		</ul>
 		<div id='trader'></div>
 	";
+# background-image : url('lib/images/wood1.jpg');
+# background-image : url('lib/images/slate1.jpg');
+
 	$style = "
 		<style>
-				background-image : url('lib/images/wood1.jpg');
-				background-image : url('lib/images/slate1.jpg');
+				.pair
+				{
+					border : 1px solid gray;
+					margin : 10px;
+					padding : 10px;
+				}
+				.pair > div:nth-child(1)
+				{
+					width : 200px;
+				}
+				.pair > div:nth-child(2)
+				{
+					margin-left : 100px;
+				}
+				.pair table
+				{
+					border : 1px solid green;
+				}
+				.pair table td
+				{
+					padding : 10px;
+				}
+				.pair table thead td
+				{
+					background : green;
+					color : white;
+					text-align : center;
+				}
 		</style>
 	";
 	$script = "
 		<script>
 			$.post('index.php?/trader','',function(data){
-				$('#trader').html(data);
+				var obj = $.parseJSON(data)
+				var html = '';
+				obj.forEach(function(a){
+					html += '<div class=\'pair\'>';
+					html += '<div>' + a[0] + ' ' + a[1] + '</div>';
+					var list = a[2];
+
+var count = 0;
+var trades = '<table>';
+trades += '<thead>';
+trades += '<tr>';
+trades += '<td>Index</td>';
+trades += '<td>Type</td>';
+trades += '<td>Time</td>';
+trades += '<td>Elapsed</td>';
+trades += '<td>Price</td>';
+trades += '<td>Amount</td>';
+trades += '<td>Date</td>';
+trades += '</tr>';
+trades += '</thead>';
+var tStart = list[0].time_stamp;
+list.forEach(function(b){
+	count++;
+	trades += '<tr>';
+	trades += '<td>' + count + '</td>';
+	trades += '<td>' + b.type + '</td>';
+	trades += '<td>' + b.time_stamp + '</td>';
+	trades += '<td>' + (b.time_stamp - tStart) + '</td>';
+	trades += '<td>' + b.price + '</td>';
+	trades += '<td>' + b.amount + '</td>';
+	trades += '<td>' + b.date + '</td>';
+	trades += '</tr>';
+});
+trades += '</table>';
+
+					html += '<div>' + trades + '</div>';
+					html += '</div>';
+				});
+				$('#trader').html(html);
+				//$('#trader').html(data);
 			});
 /*
 			$.post('index.php?/stout/getBter','',function(data){
