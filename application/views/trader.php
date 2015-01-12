933e78276996ee75aeb044133e33f9cdeaf33b0f
JamesLeB <james@gowanustech.com>
2015-01-12 14:47:19 -0500

    adjust trader view

--- application/views/trader.php
+++ application/views/trader.php
@@ -1,78 +1,73 @@
+<div id='trader'></div>
+<style>
+	/* background-image : url('lib/images/wood1.jpg'); */
+	#trader
+	{
+		background : green;
+		border : 2px solid black;
+	}
+	.pair
+	{
+		background-image : url('lib/images/slate1.jpg');
+		border : 1px solid gray;
+		margin  : 20px;
+		padding : 20px;
+		border-radius : 20px;
+		box-shadow : 2px 2px 2px black;
+		color : white;
+	}
+	.pair > div:nth-child(1)
+	{
+		border : 1px solid white;
+		padding : 5px;
+		height : 25px;
+	}
+	.pair > div:nth-child(1) > div
+	{
+		border : 1px solid black;
+		float : left;
+		width : 200px;
+	}
+	.pair > div:nth-child(1) > div:nth-child(1)
+	{
+		width : 200px;
+	}
+	.pair > div:nth-child(1) > div:nth-child(2)
+	{
+		width : 200px;
+	}
+	.pair > div:nth-child(2)
+	{
+		height : 50px;
+		border : 1px solid white;
+		width : 900px;
+		padding : 10px;
+		margin-left   : auto;
+		margin-right  : auto;
+		margin-top    : 10px;
+		margin-bottom :  5px;
+	}
+</style>
+<script>
+	$.post('index.php?/trader','',function(data){
+		var obj = $.parseJSON(data)
+		var html = '';
+		obj.forEach(function(a){
+			html += '<div class=\'pair\'>';
+			 html += '<div>';
+			  html += '<div>' + a[0] + '</div>';
+			  html += '<div>' + a[1] + '</div>';
+			 html += '</div>';
+			 html += '<div>';
+			  html += '<div>' + a[2] + '</div>';
+			 html += '</div>';
+			html += '</div>';
+		});
+		$('#trader').html(html);
+		//$('#trader').html(data);
+	});
+</script>
 <?php
-	$html = "
-		<ul>
-			<li>Open database connection</li>
-			<li>Get List of coins</li>
-			<li>Create table view of coins</li>
-		</ul>
-		<div id='trader'></div>
-	";
-# background-image : url('lib/images/wood1.jpg');
-# background-image : url('lib/images/slate1.jpg');
-
-	$style = "
-		<style>
-				.pair
-				{
-					border : 1px solid gray;
-					margin : 10px;
-					padding : 10px;
-				}
-				.pair > div:nth-child(1)
-				{
-					height : 30px;
-				}
-				.pair > div:nth-child(1) > div
-				{
-					border : 1px dotted gray;
-					float : left;
-					width : 200px;
-				}
-				.pair > div:nth-child(1) > div:nth-child(1)
-				{
-					width : 200px;
-				}
-				.pair > div:nth-child(1) > div:nth-child(2)
-				{
-					width : 200px;
-				}
-				.pair > div:nth-child(2)
-				{
-					height : 50px;
-					border : 1px solid green;
-					width : 900px;
-					padding : 10px;
-					margin-left   : auto;
-					margin-right  : auto;
-					margin-top    : 10px;
-					margin-bottom :  5px;
-				}
-		</style>
-	";
-	$script = "
-		<script>
-			$.post('index.php?/trader','',function(data){
-				var obj = $.parseJSON(data)
-				var html = '';
-				obj.forEach(function(a){
-					html += '<div class=\'pair\'>';
-					 html += '<div>';
-					  html += '<div>' + a[0] + '</div>';
-					  html += '<div>' + a[1] + '</div>';
-					 html += '</div>';
-					 html += '<div>';
-					  html += '<div>' + a[2] + '</div>';
-					 html += '</div>';
-					html += '</div>';
-				});
-				$('#trader').html(html);
-				//$('#trader').html(data);
-			});
-		</script>
-	";
-	echo $style;
-	echo $html;
-	echo $script;
 ## TEMP
 /*
 var list = a[2];
