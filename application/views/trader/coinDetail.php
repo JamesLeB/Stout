f2578b52247424309bf304b95c2ca2ea9a1753b2
JamesLeB <james@jamesleblanc.org>
2015-01-29 12:15:21 -0500

    add coin detail view

---
+++ application/views/trader/coinDetail.php
@@ -0,0 +1,20 @@
+<div id='detailCoinView'>
+	<button id='switchBack'>Close</button>
+</div>
+<style>
+	#detailCoinView
+	{
+		height : 600px;
+		width  : 800px;
+		background : white;
+		margin-top : 40px;
+		margin-left : auto;
+		margin-right : auto;
+		border : 1px solid black;
+		border-radius : 20px;
+		box-shadow : 2px 2px 2px black;
+	}
+</style>
+<script>
+	$('#switchBack').click(function(){ switchBack(); });
+</script>
