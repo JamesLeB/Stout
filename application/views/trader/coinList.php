5635609817327cf81ad6fa33246e64c70988d43a
JamesLeB <james@gowanustech.com>
2015-01-30 23:51:17 -0500

    build coin list view

--- application/views/trader/coinList.php
+++ application/views/trader/coinList.php
@@ -4,17 +4,20 @@
 
 	$html .= "<div>";
 	$html .= "<div>";
-	$html .= "<div>Coin</div>";
-	$html .= "<div>Slope</div>";
+	$html .= "<div><div>Coin</div></div>";
+	$html .= "<div><div>Slope</div></div>";
+	$html .= "<div><div>Slope48</div></div>";
+	$html .= "<div><div>Slope24</div></div>";
+	$html .= "<div><div>Volume</div></div>";
 	$html .= "</div>";
 	$html .= "</div>";
 
 	$html .= "<div>";
 	foreach($list as $item)
 	{
-		$html .= "<div>";
-		$html .= "<div>$item</div>";
-		$html .= "<div>x</div>";
+		$html .= "<div class='pair'>";
+		$html .= "<div><div>$item</div></div>";
+		$html .= "<div><div>x</div></div>";
 		$html .= "</div>";
 	}
 	$html .= "</div>";
@@ -33,33 +36,67 @@
 		border-radius : 25px;
 		box-shadow : 2px 2px 2px black;
 	}
-	#coinList div
-	{
-		border : dashed 1px gray;
-	}
-	#coinList > div { border : 2px solid red; }
 	#coinList > div > div
 	{
-		height : 40px;
+		height : 35px;
 	}
 	#coinList > div > div > div
 	{
-		width : 100px;
 		float : left;
+		margin-right : 10px;
 	}
-	#coinList > div > div > div:nth-child(1) { width : 200px; }
+	#coinList > div > div > div:nth-child(1) { width : 150px; }
 	#coinList > div > div > div:nth-child(2) { width : 100px; }
+	#coinList > div > div > div:nth-child(3) { width : 100px; }
+	#coinList > div > div > div:nth-child(4) { width : 100px; }
+	#coinList > div > div > div:nth-child(5) { width : 100px; }
 
-	#coinList > div:nth-child(1) > div:nth-child(1) > div
+	#coinList > div:nth-child(1) > div > div
 	{
 		background : green;
 		color : white;
+		height : 30px;
+	}
+	#coinList > div:nth-child(1) > div > div > div
+	{
 		text-align : center;
+		margin-top : 5px;
+	}
+	#coinList > div:nth-child(2) > div > div > div
+	{
+		margin-left : 20px;
+		margin-top : 5px;
+		height : 30px;
 	}
 	#coinList > div:nth-child(2)
 	{
 		height : 200px;
 		overflow : auto;
 	}
+	#coinList > div:nth-child(2) > div > div:nth-child(1) > div
+	{
+	}
 
 </style>
+<script>
+	$('#coinList > div:nth-child(2) > div > div:nth-child(1) > div').mouseenter(function(){
+		$(this).css('color','blue');
+	});
+	$('#coinList > div:nth-child(2) > div > div:nth-child(1) > div').mouseleave(function(){
+		$(this).css('color','black');
+	});
+	$('#coinList > div:nth-child(2) > div > div:nth-child(1) > div').click(function(){
+
+var b = '';
+var pairs = $('.pair');
+pairs.each(function(index,element){
+	var coin = $(element).children(':nth-child(1)').children(':nth-child(1)').html();
+	b += coin + '_<br/>';
+});
+
+		var parm = {groot: b};
+		$.post('index.php?/trader/getCoinDetail',parm,function(data){
+			switchScreen(data);
+		});
+	});
+</script>
