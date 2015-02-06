2eb0014e9f4405081b8b23927e6ad7b6d4d21234
JamesLeB <james@jamesleblanc.org>
2015-02-06 16:22:48 -0500

    reformat view

--- application/views/junior/loadClaims.php
+++ application/views/junior/loadClaims.php
@@ -1,6 +1,6 @@
 <div id='loadClaims'>
 	<div></div>
-	<div>Status</div>
+	<div><div></div></div>
 	<div></div>
 </div>
 <style>
@@ -8,7 +8,7 @@
 	{
 		border : 10px ridge yellow;
 		background : lightgreen;
-		height : 500px;
+		height : 800px;
 	}
 	#loadClaims > div
 	{
@@ -17,10 +17,14 @@
 		margin : 20px;
 		background : white;
 		box-shadow : 2px 2px 2px black;
+		height : 400px;
+		width : 400px;
+		overflow : auto;
 	}
-	#loadClaims > div:nth-child(1) { height : 400px; width : 400px; overflow : auto; }
-	#loadClaims > div:nth-child(2) { height : 200px; width : 600px; padding : 10px; }
-	#loadClaims > div:nth-child(3) { height : 400px; width : 400px; overflow : auto; }
+	#loadClaims > div:nth-child(1) { }
+	#loadClaims > div:nth-child(2) { width : 500px; }
+	#loadClaims > div:nth-child(2) > div:nth-child(1) { margin : 10px; }
+	#loadClaims > div:nth-child(3) { }
 	.claimFile { margin : 10px; margin-left : 20px;}
 	.processedClaimFile { margin : 10px; margin-left : 20px;}
 </style>
@@ -33,13 +37,12 @@
 			obj.forEach(function(o){
 				list += '<div class=\'claimFile\'>'+o+'</div>';
 			});
-			$('#loadClaims > div:nth-child(1)').html('');
 			$('#loadClaims > div:nth-child(1)').html(list);
 			$('.claimFile').click(function(){
 				var fileName = $(this).html();
 				var parm = { file: fileName };
 				$.post('index.php?/junior/processClaim',parm,function(data){
-					$('#loadClaims > div:nth-child(2)').html(data);
+					$('#loadClaims > div:nth-child(2) div:nth-child(1)').html(data);
 					getClaims();
 				});
 			});
@@ -56,13 +59,12 @@
 			obj.forEach(function(o){
 				list += '<div class=\'processedClaimFile\'>'+o+'</div>';
 			});
-			$('#loadClaims > div:nth-child(3)').html('');
 			$('#loadClaims > div:nth-child(3)').html(list);
 			$('.processedClaimFile').click(function(){
 				var fileName = $(this).html();
 				var parm = { file: fileName };
 				$.post('index.php?/junior/resetClaim',parm,function(data){
-					$('#loadClaims > div:nth-child(2)').html(data);
+					$('#loadClaims > div:nth-child(2) > div:nth-child(1)').html(data);
 					getClaims();
 				});
 			});
@@ -75,5 +77,5 @@
 		});
 	}
 	getClaims();
-	$('#loadClaims > div:nth-child(2)').html('status goes here');
+	$('#loadClaims > div:nth-child(2) > div:nth-child(1)').html('Welcome');
 </script>
