ad7c03c76684afa1320993ed854fc5bab98b1853
JamesLeB <james@jamesleblanc.org>
2015-02-10 11:10:09 -0500

    reformat claim loading screen

--- application/views/junior/loadClaims.php
+++ application/views/junior/loadClaims.php
@@ -8,23 +8,24 @@
 	{
 		border : 10px ridge yellow;
 		background : lightgreen;
-		height : 800px;
+		height : 680px;
+		position : relative;
 	}
 	#loadClaims > div
 	{
 		border : 1px solid black;
-		float : left;
 		margin : 20px;
 		background : white;
 		box-shadow : 2px 2px 2px black;
-		height : 400px;
+		height : 300px;
 		width : 400px;
 		overflow : auto;
+		position : absolute;
 	}
 	#loadClaims > div:nth-child(1) { }
-	#loadClaims > div:nth-child(2) { width : 500px; }
+	#loadClaims > div:nth-child(2) { left: 500px; width: 800px; height: 600px;}
 	#loadClaims > div:nth-child(2) > div:nth-child(1) { margin : 10px; }
-	#loadClaims > div:nth-child(3) { }
+	#loadClaims > div:nth-child(3) { top: 330px;}
 	.claimFile { margin : 10px; margin-left : 20px;}
 	.processedClaimFile { margin : 10px; margin-left : 20px;}
 </style>
