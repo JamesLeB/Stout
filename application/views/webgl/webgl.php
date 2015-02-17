e6545231dd8bb19bb491a1d638940f32a696acd2
JamesLeB <james@gowanustech.com>
2015-02-16 21:56:46 -0500

    add rotation to plane

--- application/views/webgl/webgl.php
+++ application/views/webgl/webgl.php
@@ -1,4 +1,5 @@
 <script type="text/javascript" src="v/webgl/glMatrix.js"></script>
+<script type="text/javascript" src="v/webgl/webgl-utils.js"></script>
 <script id="shader-fs" type="x-shader/x-fragment">
     precision mediump float;
 	varying vec4 vColor;
@@ -73,11 +74,27 @@
         shaderProgram.mvMatrixUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
     }
     var mvMatrix = mat4.create();
+	var mvMatrixStack = [];
     var pMatrix  = mat4.create();
+	function mvPushMatrix(){
+		var copy = mat4.create();
+		mat4.set(mvMatrix, copy);
+		mvMatrixStack.push(copy);
+	}
+	function mvPopMatrix(){
+		if(mvMatrixStack.length == 0){
+			throw "Invalid popMatrix!";
+		}
+		mvMatrix = mvMatrixStack.pop();
+	}
     function setMatrixUniforms() {
         gl.uniformMatrix4fv(shaderProgram.pMatrixUniform, false, pMatrix);
         gl.uniformMatrix4fv(shaderProgram.mvMatrixUniform, false, mvMatrix);
     }
+	function degToRad(degrees)
+	{
+		return degrees * Math.PI / 180;
+	}
     var grootPos;
     var grootCol;
     function initBuffers() {
@@ -95,27 +112,57 @@
 
         grootCol = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, grootCol);
+        colors = [
+             0.0, 0.99, 1.0, 1.0,
+             0.0, 0.80, 1.0, 1.0,
+             0.0, 0.80, 1.0, 1.0,
+             0.0, 0.99, 1.0, 1.0
+		];
+/*
         colors = [];
 		for (var i=0; i<4; i++){
 			colors = colors.concat([0.0,0.4,0.0,0.9]);
 		}
+*/
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
         grootCol.itemSize = 4;
         grootCol.numItems = 4;
     }
+	var rSquare = 0;
     function drawScene() {
         gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
         mat4.perspective(45, gl.viewportWidth / gl.viewportHeight, 0.1, 100.0, pMatrix);
         mat4.identity(mvMatrix);
         mat4.translate(mvMatrix, [0.0, 0.0, -6.0]);
+		mvPushMatrix();
+		mat4.rotate(mvMatrix, degToRad(rSquare), [0, 1, 0]);
+		mat4.rotate(mvMatrix, degToRad(rSquare), [1, 0, 0]);
+		mat4.rotate(mvMatrix, degToRad(rSquare), [0, 0, 1]);
         gl.bindBuffer(gl.ARRAY_BUFFER, grootPos);
         gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, grootPos.itemSize, gl.FLOAT, false, 0, 0);
         gl.bindBuffer(gl.ARRAY_BUFFER, grootCol);
         gl.vertexAttribPointer(shaderProgram.vertexColorAttribute, grootCol.itemSize, gl.FLOAT, false, 0, 0);
         setMatrixUniforms();
         gl.drawArrays(gl.TRIANGLE_STRIP, 0, grootPos.numItems);
+		mvPopMatrix();
     }
+	var lastTime = 0;
+	function animate()
+	{
+		var timeNow = new Date().getTime();
+		if(lastTime != 0){
+			var elapsed = timeNow - lastTime;
+			rSquare += (75 * elapsed) / 1000;
+		}
+		lastTime = timeNow;
+	}
+	function tick()
+	{
+		requestAnimFrame(tick);
+		drawScene();
+		animate();
+	}
     function webGLStart() {
         var canvas = document.getElementById("canvas");
         initGL(canvas);
@@ -123,7 +170,7 @@
         initBuffers();
         gl.clearColor(0.0, 0.0, 0.0, 1.0);
         gl.enable(gl.DEPTH_TEST);
-        drawScene();
+		tick();
     }
 	$(document).ready(function(){
 		webGLStart();
