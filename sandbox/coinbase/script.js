79cda40465b64b26a0ea30e7866f7196a9b8ead1
JamesLeB <james@gowanustech.com>
2015-03-04 20:58:17 -0500

    set up to pass data to home

--- sandbox/coinbase/script.js
+++ sandbox/coinbase/script.js
@@ -8,6 +8,27 @@ $(document).ready(function(){
 			$('#status').css('background','lightgreen');
 			var obj = $.parseJSON(data);
 			$('#status').html(obj.status);
+			var usdAmount  = '#exchange > div:nth-child(1) > div:nth-child(2) > div:nth-child(2)';
+			var btcAmount  = '#exchange > div:nth-child(1) > div:nth-child(3) > div:nth-child(2)';
+
+			var bookBid    = '#exchange > div:nth-child(4) > div:nth-child(2) > div:nth-child(1)';
+			var bookSpread = '#exchange > div:nth-child(4) > div:nth-child(2) > div:nth-child(2)';
+			var bookAsk    = '#exchange > div:nth-child(4) > div:nth-child(2) > div:nth-child(3)';
+
+			var traderBid  = '#exchange > div:nth-child(2) > div:nth-child(2) > div:nth-child(2)';
+			var traderAsk  = '#exchange > div:nth-child(2) > div:nth-child(2) > div:nth-child(5)';
+			var traderSize = '#exchange > div:nth-child(2) > div:nth-child(3) > div:nth-child(2)';
+
+			$(usdAmount).css('border','1px solid yellow');
+			$(btcAmount).css('border','1px solid yellow');
+
+			$(bookBid).css('border','1px solid yellow');
+			$(bookSpread).css('border','1px solid yellow');
+			$(bookAsk).css('border','1px solid yellow');
+
+			$(traderBid).css('border','1px solid yellow');
+			$(traderAsk).css('border','1px solid yellow');
+			$(traderSize).css('border','1px solid yellow');
 		});
 	},1000);
 });
