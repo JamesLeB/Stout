8c8c29efe7e60416f5a977655498fec1ce6a2bb7
JamesLeB <james@jamesleblanc.org>
2015-03-10 14:33:49 -0400

    remove old code

--- sandbox/coinbase/time.php
+++ sandbox/coinbase/time.php
@@ -1,18 +1,11 @@
 <?php
 	require_once('trader.php');
 	$trader = new trader();
-	$accounts = '';#$trader->getAccounts();
 	$book     = $trader->getOrderBook();
 	$orders   = $trader->getOrders();
 
-	#$k = $accounts['usdBalance'];
-	$openBids = '';#$trader->checkOpenBids($k);
-
 	echo json_encode(array(
-		'status' => 'running',
-		'accounts' => $accounts,
 		'book' => $book,
-		'orders' => $orders,
-		'openBids' => $openBids
+		'orders' => $orders
 	));
 ?>
