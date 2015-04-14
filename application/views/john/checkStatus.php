b62d9e51604e4b06b6f13fdabafebb7117eac7a3
JamesLeB <james@jamesleblanc.org>
2015-04-14 11:25:20 -0400

    developing create claim status request functino

--- application/views/john/checkStatus.php
+++ application/views/john/checkStatus.php
@@ -1,3 +1,25 @@
-<div>
+<div id='claimStatusRequest'>
 	Lets create a claim status request
+	<div></div>
+	<div></div>
 </div>
+<style>
+	#claimStatusRequest
+	{
+		border: 5px ridge yellow;
+		background: lightgreen;
+		height:  600px;
+		width:  1000px;
+	}
+	#claimStatusRequest > div
+	{
+		border: 5px ridge gray;
+		background: lightgray;
+		margin: 10px;
+		height: 200px;
+	}
+</style>
+<script>
+	$('#claimStatusRequest > div:nth-child(1)').html('getting list from somewhere');
+	$('#claimStatusRequest > div:nth-child(2)').html('Click out put will go here');
+</script>
