b23051d3309afe5770071714b0b05a4fef02fb8c
JamesLeB <james@jamesleblanc.org>
2015-06-30 16:37:50 -0400

    adding files

---
+++ sandbox/template/cont.php
@@ -0,0 +1,4 @@
+<?php
+	$a = $_POST['james'];
+	echo "this is the controler return $a";
+?>
--- sandbox/template/template.php
+++ sandbox/template/template.php
@@ -8,7 +8,11 @@
 	<script>
 		$(document).ready(function(){
 			//alert('Document Ready');
+			var p = { james: 'kara' };
 			$('#tabs').tabs({active:0});
+			$.post('cont.php',p,function(data){
+				$('#header').html(data);
+			});
 		});
 	</script>
 	<style>
@@ -26,10 +30,13 @@
 		}
 	</style>
 </head>
+<?php
+	$page1 = file_get_contents('start.html');
+?>
 <body>
 	<div id='wrapper'>
 		<!-- HEADER --!>
-		<div>
+		<div id='header'>
 			Header
 		</div>
 		<!-- BODY --!>
@@ -39,12 +46,8 @@
 					<li><a href='#tab1'>One</a></li>
 					<li><a href='#tab2'>Two</a></li>
 				</ul>
-				<div id='tab1'>
-					First tab
-				</div>
-				<div id='tab2'>
-					Second tab
-				</div>
+				<div id='tab1'> <?php echo $page1; ?> </div>
+				<div id='tab2'> Second tab </div>
 			</div>
 		</div>
 		<!-- FOOTER --!>
