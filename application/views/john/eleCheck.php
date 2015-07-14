844bbaccc212c214f243aa55634d37a5ed4add9c
JamesLeB <james@jamesleblanc.org>
2015-07-14 13:57:28 -0400

    add buttons to create ele file and read ele return

--- application/views/john/eleCheck.php
+++ application/views/john/eleCheck.php
@@ -1,7 +1,25 @@
+<button id='goButton'>GO</button>
+<button id='goRead'>Read</button>
 <div id='newJob'></div>
 <script>
-	$.post('index.php?/john/getnewjob','',function(d)
+	$('#goButton').click(function()
 	{
-		$('#newJob').html(d);
+		$.post('index.php?/john/getnewjob','',function(d)
+		{
+			$('#newJob').html(d);
+		});
+	});
+	$('#goRead').click(function()
+	{
+		$.post('index.php?/john/getnewjobReadJson','',function(d)
+		{
+			$('#newJob').html(d);
+		});
 	});
 </script>
+<style>
+	#newJob table td
+	{
+		padding: 10px;
+	}
+</style>
